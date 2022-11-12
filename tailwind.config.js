cfbd2c8ca0e915f6a44338b5fb546c719b974ab0
developer-sahadat <mdridoysikder862@gmail.com>
2022-11-13 00:16:10 +0600

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,19 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        george: ['Outfit', 'sans-serif'],
-        syne: ["Syne", 'sans-serif']
-      }
+        george: ["Outfit", "sans-serif"],
+        syne: ["Syne", "sans-serif"],
+      },
+      colors: {
+        transparent: "transparent",
+        primary: "#ff317b",
+        secondary: "#5650ce",
+        success: "#2d4186",
+        warning: "#9aace5",
+        info: "#b3d4fc",
+        white: "#ffffff",
+      },
     },
   },
   plugins: [require("daisyui")],
-}
+};
