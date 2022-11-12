d8ed39f1433f692e64a7a232cd2d2b75e488e71a
pallobbiswas <pallobbiswas326@gmail.com>
2022-11-12 20:59:02 +0600

    login done

--- src/route/Index.js
+++ src/route/Index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from '../pages/Login';
 import SingleProfile from '../pages/SingleProfile';
 
 const Index = () => {
@@ -7,7 +8,8 @@ const Index = () => {
         <div>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<SingleProfile />}></Route>
+                    <Route path="/" element={<SingleProfile />} />
+                    <Route path="/login" element={<Login />} />
                 </Routes>
             </BrowserRouter>
         </div>
