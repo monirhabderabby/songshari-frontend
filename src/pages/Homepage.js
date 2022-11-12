76de2792366fad4b16be5990976ea08a484dcf6d
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-11-12 21:14:59 +0600

    three easy steps almost done with banner overlay edited

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Banner from '../components/Homepage/Banner';
+import ThreeEasySteps from '../components/Homepage/ThreeEasySteps';
 
 const Homepage = () => {
     return (
         <div>
             <Banner></Banner>
+            <div>
+                <ThreeEasySteps></ThreeEasySteps>
+            </div>
         </div>
     );
 };
