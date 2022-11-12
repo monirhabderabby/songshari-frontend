fbcfb460feef98cafee5a5bd185a3f2825680b3b
AnikHaque <ekramul15-811@diu.edu.bd>
2022-11-12 23:32:11 +0600

    updated wrapper & pages

---
+++ src/Wrapper/Profileinfo/ProfileInfo.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+ const ProfileInfo = () => {
+  return (
+    <div>ProfileInfo</div>
+  )
+}
+export default ProfileInfo;
--- src/route/Index.js
+++ src/route/Index.js
@@ -5,6 +5,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "../Wrapper/About/About";
 import Homepage from "../Wrapper/Home/Homepage";
 import SingleProfiles from "../components/SingleProfilesUser/SingleProfiles";
+import MemberShip from "../Wrapper/Membership/MemberShip";
+import FindPartner from "../Wrapper/Findpartner/FindPartner";
+import ProfileInfo from "../Wrapper/Profileinfo/ProfileInfo";
+import Course from "../Wrapper/Course/Course";
+import FindAlawyer from "../Wrapper/Findalawyer/FindAlawyer";
+import Kazi from "../Wrapper/Kazi/Kazi";
+import Agent from "../Wrapper/Agent/Agent";
 
 const Index = () => {
     return (
@@ -14,6 +21,13 @@ const Index = () => {
                     <Route path="/" element={<Homepage></Homepage>}></Route>
                     <Route path="/about" element={<About></About>}></Route>
                     <Route path="/userprofile" element={<SingleProfiles></SingleProfiles>}></Route>
+                    <Route path="/membership" element={<MemberShip></MemberShip>}></Route>
+                    <Route path="/findpartner" element={<FindPartner></FindPartner>}></Route>
+                    <Route path="/profileinfo" element={<ProfileInfo></ProfileInfo>}></Route>
+                    <Route path="/course" element={<Course></Course>}></Route>
+                    <Route path="/findalawyer" element={<FindAlawyer></FindAlawyer>}></Route>
+                    <Route path="/kazi" element={<Kazi></Kazi>}></Route>
+                    <Route path="/agent" element={<Agent></Agent>}></Route>
                     
                 </Routes>
             </BrowserRouter>
