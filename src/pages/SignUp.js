d955a385177cc510442b5ff5f6d3b9a24161c043
pallobbiswas <pallobbiswas326@gmail.com>
2022-11-12 22:07:55 +0600

    ragistration added

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
-import loginGif from "../../src/assets/images/gif/teddy.gif";
-import google from "../../src/assets/images/icons/google.svg";
+import ragistrationImg from "../../src/assets/images/gif/couple.gif";
 
-const Login = () => {
+const SignUp = () => {
     const {
         register,
         handleSubmit,
@@ -17,16 +16,16 @@ const Login = () => {
         reset()
     }
     return (
-        <div className='bg-gray-100 '>
-            <div className="grid grid-cols-2 justify-center items-center container mx-auto">
-                <div style={{ width: "100%", }}>
-                    <img style={{ width: "90%" }} className='rounded-md' src={loginGif} alt="login image" />
+        <div>
+            <div className='grid grid-cols-2 justify-center items-center container mx-auto'>
+                <div>
+                    <img className='rounded-lg' src={ragistrationImg} alt="Ragistration image" />
                 </div>
                 <div>
-                    <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
+                    <div className="relative flex flex-col justify-center min-h-screen overflow-x-scroll">
                         <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
                             <h1 className="text-3xl font-semibold text-center text-purple-700 underline">
-                                Sign in
+                                Sign up
                             </h1>
                             <form onSubmit={handleSubmit(onSubmit)} className="mt-6">
                                 <div className="mb-2">
@@ -55,41 +54,18 @@ const Login = () => {
                                     />
                                     {errors.password && <p className='text-red-500'>password is required.</p>}
                                 </div>
-                                <a
-                                    href="#"
-                                    className="text-xs text-purple-600 hover:underline"
-                                >
-                                    Forget Password?
-                                </a>
-
-
                                 <div className="mt-6">
-                                    <input className="cursor-pointer  w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600" type="submit" value='Login' />
+                                    <input className="cursor-pointer  w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600" type="submit" value='Ragistration' />
                                 </div>
                             </form>
-                            <div class="relative flex py-5 items-center">
-                                <div class="flex-grow border-t border-gray-400"></div>
-                                <span class="flex-shrink mx-4 text-gray-400">Or</span>
-                                <div class="flex-grow border-t border-gray-400"></div>
-                            </div>
-                            <div className="mt-2">
-                                <p className="mb-4">Sign up with your email</p>
-                                <div className=' w-full flex justify-center'>
-                                    <button className="flex items-center justify-center cursor-pointer w-3/4 px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-pink-500 rounded-full hover:bg-pink-600 focus:outline-none focus:bg-pink-600">
-                                        <img style={{ width: "40px" }} src={google} alt="google logo" />
-                                        <p className='pl-4'> Login form google</p>
-                                    </button>
-                                </div>
-                            </div>
-
                             <p className="mt-8 text-xs font-light text-center text-gray-700">
                                 {" "}
-                                Don't have an account?{" "}
+                                Already have an account?{" "}
                                 <Link
-                                    to="/signup"
+                                    to="/login"
                                     className="font-medium text-purple-600 hover:underline"
                                 >
-                                    Sign up
+                                    Sign in
                                 </Link>
                             </p>
                         </div>
@@ -100,4 +76,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default SignUp;
--- src/route/Index.js
+++ src/route/Index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from '../pages/Login';
+import SignUp from '../pages/SignUp';
 import SingleProfile from '../pages/SingleProfile';
 
 const Index = () => {
@@ -10,6 +11,7 @@ const Index = () => {
                 <Routes>
                     <Route path="/" element={<SingleProfile />} />
                     <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<SignUp />} />
                 </Routes>
             </BrowserRouter>
         </div>
