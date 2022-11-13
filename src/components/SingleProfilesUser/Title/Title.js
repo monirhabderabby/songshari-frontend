81890c7a27076bce2d9d98ddb409cc647b37edd7
Monir <monirhrabby.personal@gmail.com>
2022-11-13 23:18:35 +0600

    resolve conflicts

--- src/components/SingleProfilesUser/Title/Title.js
+++ src/components/SingleProfilesUser/Title/Title.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import "../../../assets/css/title.css";
-
-const Title = ({titleText}) => {
-    return (
-        <div className='py-2 text-left'>
-            <h1 className=' text-purple text-3xl'>{titleText}</h1>
-            <h1 className='half-border my-2'></h1>
-=======
 import React from "react";
 import "../../../assets/css/title.css";
 
@@ -16,14 +6,9 @@ const Title = ({ titleText }) => {
         <div className="py-2 text-left">
             <h1 className=" text-purple text-3xl">{titleText}</h1>
             <h1 className="half-border my-2"></h1>
->>>>>>> main
             <hr />
         </div>
     );
 };
 
-<<<<<<< HEAD
 export default Title;
-=======
-export default Title;
->>>>>>> main
