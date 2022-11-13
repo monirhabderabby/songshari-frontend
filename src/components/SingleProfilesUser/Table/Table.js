e6bbafa7578334635c6e635a3362f0895364c984
Monir <monirhrabby.personal@gmail.com>
2022-11-13 08:12:30 +0600

    resolve conflicts

---
+++ src/components/SingleProfilesUser/Table/Table.js
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from "react";
+import "../../../assets/css/table.css";
+import Title from "../Title/Title";
+
+const Table = () => {
+    const [tables, setTables] = useState([]);
+    useEffect(() => {
+        fetch("json/tableData.json")
+            .then(res => res.json())
+            .then(data => {
+                setTables(data);
+            });
+    }, []);
+    return (
+        <div>
+            <div className="pt-5">
+                <div className="notification-bar">
+                    <div class="flex justify-center items-center gap-4">
+                        <button class="mt-4 font-medium leading-tight text-gray-800">Active</button>
+                        <button class="mt-4 font-medium leading-tight text-gray-800">Notify</button>
+                        <button class="mt-4 font-medium leading-tight text-gray-800">
+                            Example
+                            <span class="inline-block px-1 leading-none text-center whitespace-nowrap align-baseline font-bold bg-pink-600 text-white rounded">
+                                1
+                            </span>
+                        </button>
+                        <button class="mt-4 font-medium leading-tight text-gray-800">
+                            heading
+                            <span class="inline-block px-1 leading-none text-center whitespace-nowrap align-baseline font-bold bg-pink-600 text-white rounded">
+                                1
+                            </span>
+                        </button>
+                        <button class="mt-4 font-medium leading-tight text-gray-800">
+                            Message
+                            <span class="inline-block px-1 leading-none text-center whitespace-nowrap align-baseline font-bold bg-pink-600 text-white rounded">
+                                1
+                            </span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            {/* table 1 */}
+            <div className="my-6 py-6 table-bar px-8">
+                <div>
+                    <Title titleText="This is first Table"></Title>
+                </div>
+                {tables.map(t => {
+                    return (
+                        <div className="">
+                            <div className="flex justify-between custom-design">
+                                <h1>{t.name}</h1>
+                                <h1>{t.title}</h1>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+            <div class="w-full mt-10 bg-white px-4 py-6 rounded-lg shadow ">
+                <Title titleText="Myself Summary"></Title>
+                <div class="">
+                    <div class="text-left">
+                        <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate blanditiis cum amet corrupti! Repellendus veritatis
+                            dicta reiciendis eligendi, vero ea nemo. Adipisci tenetur quis neque assumenda a libero maiores eaque?
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="my-6 py-6 table-bar px-8">
+                <div>
+                    <Title titleText="This is third Table"></Title>
+                </div>
+                {tables.map(t => {
+                    return (
+                        <div className="">
+                            <div className="flex justify-between custom-design">
+                                <h1>{t.name}</h1>
+                                <h1>{t.title}</h1>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+            <div className="my-6 py-6 table-bar px-8">
+                <div>
+                    <Title titleText="This is second Table"></Title>
+                </div>
+                {tables.map(t => {
+                    return (
+                        <div className="">
+                            <div className="flex justify-between custom-design">
+                                <h1>{t.name}</h1>
+                                <h1>{t.title}</h1>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default Table;
--- src/components/pages/Banner/Banner.js
+++ src/components/pages/Banner/Banner.js
@@ -1,44 +1,40 @@
-import React from 'react';
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // import required modules
-import { Autoplay, Pagination, Navigation } from "swiper";
+import { Autoplay, Navigation, Pagination } from "swiper";
 
-import slider from '../../../assets/Images/Slider/Slider.jpeg';
-import slider1 from '../../../assets/Images/Slider/Slider1.jpeg';
-import slider2 from '../../../assets/Images/Slider/Slider2.jpeg';
-import slider3 from '../../../assets/Images/Slider/Slider3.jpeg';
-import slider4 from '../../../assets/Images/Slider/Slider4.jpeg';
-import slider5 from '../../../assets/Images/Slider/Slider5.jpeg';
-import slider6 from '../../../assets/Images/Slider/Slider6.jpeg';
-import slider7 from '../../../assets/Images/Slider/Slider7.jpeg';
-import SwipeMatch from './SwipeMatch';
-import FindPartnerForm from './FindPartnerForm';
+import slider from "../../../assets/images/Slider/Slider.jpeg";
+import slider1 from "../../../assets/Images/Slider/Slider1.jpeg";
+import slider2 from "../../../assets/Images/Slider/Slider2.jpeg";
+import slider3 from "../../../assets/Images/Slider/Slider3.jpeg";
+import slider4 from "../../../assets/Images/Slider/Slider4.jpeg";
+import slider5 from "../../../assets/Images/Slider/Slider5.jpeg";
+import slider6 from "../../../assets/Images/Slider/Slider6.jpeg";
+import slider7 from "../../../assets/Images/Slider/Slider7.jpeg";
+import FindPartnerForm from "./FindPartnerForm";
+import SwipeMatch from "./SwipeMatch";
 
 const Banner = () => {
-
     const sliderImages = [slider, slider1, slider2, slider3, slider4, slider5, slider6, slider7];
     return (
-        <div className='relative'>
+        <div className="relative">
             <Swiper
                 spaceBetween={30}
                 centeredSlides={true}
                 loop={true}
-                autoplay={
-                    { delay: 3000 }
-                }
+                autoplay={{ delay: 3000 }}
                 // navigation={true}
                 modules={[Autoplay, Pagination, Navigation]}
                 className="h-[100vh] -z-50"
             >
-                {
-                    sliderImages.map((image, index) => <SwiperSlide key={index}>
-                        <img className='w-full' src={image} alt="" />
+                {sliderImages.map((image, index) => (
+                    <SwiperSlide key={index}>
+                        <img className="w-full" src={image} alt="" />
                     </SwiperSlide>
-                    )
-                }
+                ))}
             </Swiper>
-            <div className='absolute top-40 z-50  flex justify-around w-full'>
+            <div className="absolute top-40 z-50  flex justify-around w-full">
                 <SwipeMatch></SwipeMatch>
                 <FindPartnerForm></FindPartnerForm>
             </div>
