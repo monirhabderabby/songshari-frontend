906e562ecfce0157c37db3213758272bcf75f471
developer-sahadat <mdridoysikder862@gmail.com>
2022-11-13 22:01:28 +0600

    shop design completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import "./App.css";
+import Index from "./route/Index";
+
+//Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
 function App() {
-  return <div>dgsdfhg</div>;
+    return (
+        <div>
+            <Index></Index>
+        </div>
+    );
 }
 
 export default App;
