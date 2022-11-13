5d266e99986bead2b42cf8d76ebfedae2f97b993
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-11-13 20:25:55 +0600

    begin family journey section completed and navbar half done

--- src/Wrapper/Home/Homepage.js
+++ src/Wrapper/Home/Homepage.js
@@ -3,10 +3,12 @@ import Banner from '../../components/pages/Home/Banner/Banner';
 import ThreeEasySteps from '../../components/pages/Home/Banner/ThreeEasySteps';
 import BeginFamilyJourney from '../../components/pages/Home/BeginFamilyJourney/BeginFamilyJourney';
 import SecureVerified from '../../components/pages/Home/SecureVerified/SecureVerified';
+import NavBar from '../../components/pages/Shared/NavBar';
 
 const Homepage = () => {
     return (
         <div className='font-george'>
+            <NavBar></NavBar>
             <Banner></Banner>
             <div>
                 <ThreeEasySteps></ThreeEasySteps>
