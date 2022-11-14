42b4b18ada3690346754057adb2475a96e2ac304
pallobbiswas <pallobbiswas326@gmail.com>
2022-11-14 21:20:56 +0600

    some bug fixed

--- src/components/CardComopents/Anexecutive.js
+++ src/components/CardComopents/Anexecutive.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import "../../assets/css/anexecutive.css";
-import p1 from "../../assets/images/p1.png";
+import p1 from "../../assets/images/features/i1.png";
+import p2 from "../../assets/images/features/i2.png";
+import { default as p3, default as p4 } from "../../assets/images/features/i3.png";
 import Title from '../SingleProfilesUser/Title/Title';
 
 const Anexecutive = () => {
     return (
-        <div className='container-fluid py-10'>
+        <div className='container mx-auto py-10'>
             <div className='main-container'>
                 <div className='section-header'>
                     <div className='text-center'>
@@ -25,48 +27,44 @@ const Anexecutive = () => {
                             <div className='section-img-container z-0'>
                             </div>
                         </div>
-                        <div className='section-text-contener'>
+                        <div className='section-text-contener '>
                             <div className='flex items-start'>
-                                <div>
-                                    <img className='rounded-img' src={p1} alt="" />
+                                <div className='rounded-img' >
+                                    <img src={p1} alt="" />
                                 </div>
-                                <div className='ml-5 pb-3'>
+                                <div className='ml-5 pb-3 md:pr-20'>
                                     <Title titleText="Simple to use"></Title>
-                                    <p className='my-1'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                                    <p className='mb-2'>Lorem ipsum</p>
+                                    <p className='my-1'>Simple steps to follow to have a matching connection</p>
                                     <hr />
                                 </div>
                             </div>
                             <div className='flex items-start'>
-                                <div>
-                                    <img className='rounded-img' src={p1} alt="" />
+                                <div className='rounded-img'>
+                                    <img src={p2} alt="" />
                                 </div>
-                                <div className='ml-5 pb-3'>
-                                    <Title titleText="Simple to use"></Title>
-                                    <p className='my-1'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                                    <p className='mb-2'>Lorem ipsum</p>
+                                <div className='ml-5 pb-3 md:pr-20'>
+                                    <Title titleText="Smart matching"></Title>
+                                    <p className='my-1'>Simple steps to follow to have a matching connection</p>
                                     <hr />
                                 </div>
                             </div>
                             <div className='flex items-start'>
-                                <div>
-                                    <img className='rounded-img' src={p1} alt="" />
+                                <div className='rounded-img'>
+                                    <img src={p3} alt="" />
                                 </div>
-                                <div className='ml-5 pb-3'>
-                                    <Title titleText="Simple to use"></Title>
-                                    <p className='my-1'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                                    <p className='mb-2'>Lorem ipsum</p>
+                                <div className='ml-5 pb-3 md:pr-20'>
+                                    <Title titleText="Filter very fast"></Title>
+                                    <p className='my-1'>Simple steps to follow to have a matching connection</p>
                                     <hr />
                                 </div>
                             </div>
                             <div className='flex items-start'>
-                                <div>
-                                    <img className='rounded-img' src={p1} alt="" />
+                                <div className='rounded-img'>
+                                    <img src={p4} alt="" />
                                 </div>
-                                <div className='ml-5 pb-3'>
-                                    <Title titleText="Simple to use"></Title>
-                                    <p className='my-1'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                                    <p className='mb-2'>Lorem ipsum</p>
+                                <div className='ml-5 pb-3 md:pr-20'>
+                                    <Title titleText="Cool community"></Title>
+                                    <p className='my-1'>Simple steps to follow to have a matching connection</p>
                                     <hr />
                                 </div>
                             </div>
