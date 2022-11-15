2d50ff073ba25d337d5f44433352a981a2214daf
developer-sahadat <mdridoysikder862@gmail.com>
2022-11-15 15:23:19 +0600

    navbar

--- src/components/pages/Home/Banner/Banner.js
+++ src/components/pages/Home/Banner/Banner.js
@@ -16,30 +16,39 @@ import FindPartnerForm from "./FindPartnerForm";
 import SwipeMatch from "./SwipeMatch";
 
 const Banner = () => {
-    const sliderImages = [slider, slider1, slider2, slider3, slider4, slider5, slider6, slider7];
-    return (
-        <div className="relative">
-            <Swiper
-                spaceBetween={30}
-                centeredSlides={true}
-                loop={true}
-                autoplay={{ delay: 3000 }}
-                // navigation={true}
-                modules={[Autoplay, Pagination, Navigation]}
-                className="h-[100vh] -z-50"
-            >
-                {sliderImages.map((image, index) => (
-                    <SwiperSlide key={index}>
-                        <img className="w-full" src={image} alt="" />
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-            <div className="absolute top-40 z-50  flex justify-around w-full">
-                <SwipeMatch></SwipeMatch>
-                <FindPartnerForm></FindPartnerForm>
-            </div>
-        </div>
-    );
+  const sliderImages = [
+    slider,
+    slider1,
+    slider2,
+    slider3,
+    slider4,
+    slider5,
+    slider6,
+    slider7,
+  ];
+  return (
+    <div className="relative">
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        loop={true}
+        autoplay={{ delay: 3000 }}
+        // navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="h-[100vh] -z-50"
+      >
+        {sliderImages.map((image, index) => (
+          <SwiperSlide key={index}>
+            <img className="w-full" src={image} alt="" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className="absolute top-40 z-40  flex justify-around w-full">
+        <SwipeMatch></SwipeMatch>
+        <FindPartnerForm></FindPartnerForm>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
