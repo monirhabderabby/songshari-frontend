7abc57c277181ba1f165a9895ba8433e6483f1d1
pallobbiswas <pallobbiswas326@gmail.com>
2022-11-16 10:42:04 +0600

    responcive done

--- src/Wrapper/Community/Community.js
+++ src/Wrapper/Community/Community.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Anexecutive from '../../components/CardComopents/Anexecutive';
+import Membership from '../../components/membership/Membership';
 import Comunity from '../../components/pages/comunity/Comunity';
 
 const Community = () => {
   return (
     <div className='mb-32'>
       <Comunity />
+      <div className='my-6'>
+        <Anexecutive />
+      </div>
+      <Membership />
     </div>
   )
 }
