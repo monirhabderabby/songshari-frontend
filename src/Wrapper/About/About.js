6efd1c8966da80654f8f312396a3b3f73a0de752
pallobbiswas <pallobbiswas326@gmail.com>
2022-11-17 13:42:46 +0600

    half done

--- src/Wrapper/About/About.js
+++ src/Wrapper/About/About.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import CommonBanner from '../../components/comons/CommonBanner'
+import AboutComponents from '../../components/pages/About/AboutComponents'
+import AnimatetdBanner from '../../components/pages/About/AnimatetdBanner'
+import LoveIsFeath from '../../components/pages/About/LoveIsFeath'
+import NavBar from '../../components/pages/Shared/NavBar'
 
  const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <NavBar />
+      <CommonBanner title='About Us' currentStage='> About' />
+      <AboutComponents />
+      <AnimatetdBanner />
+      <LoveIsFeath />
+    </div>
   )
 }
 export default About
---
+++ src/components/pages/About/AnimatetdBanner.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import bgHeartShape from '../../../assets/images/HomeImage/heartshape.png';
+import soleMate from '../../../assets/images/HomeImage/soleMate.png';
+import animetedcss from './animated.module.css';
+
+const AnimatetdBanner = () => {
+    return (
+        <div className='bg-[#5736cb] relative h-96 overflow-hidden mt-5'>
+            <img className='absolute top-0' style={{ animation: `${animetedcss.scaleAni} 5s linear infinite` }} src={bgHeartShape} alt="animation image" />
+            <div className='container mx-auto flex items-center'>
+                <div className='grid grid-cols-1 md:grid-cols-2 items-center h-full w-full justify-between'>
+                    <div className='text-center md:text-left'>
+                        <h2 className='mb-4 text-2xl md:text-5xl font-bold text-white'>Best Ways to Find Your </h2>
+                        <h2 className='mb-4 text-2xl md:text-5xl font-bold text-white'>True Sole Mate</h2>
+                        <button
+                            type="submit"
+                            className="px-8 py-3 mt-4 text-1xl text-white font-semibold rounded-full shadow-xl shadow-[#0c4ea54d] cursor-pointer leading-6"
+                            style={{ backgroundImage: "linear-gradient(155deg, rgba(228, 18, 114, 1) 0%, #4844a5 100%)" }}
+                        >
+                            Join Now!
+                        </button>
+                    </div>
+                    <div className=''>
+                        <img src={soleMate} alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AnimatetdBanner;
