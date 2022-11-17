8e45aa1218596e8bc89f55061153a58e7aa187f1
pallobbiswas <pallobbiswas326@gmail.com>
2022-11-17 23:10:26 +0600

    responsive firx

--- src/Wrapper/About/About.js
+++ src/Wrapper/About/About.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Anexecutive from '../../components/CardComopents/Anexecutive'
+import MeetCard from '../../components/CardComopents/MeetCard'
 import CommonBanner from '../../components/comons/CommonBanner'
+import Membership from '../../components/membership/Membership'
 import AboutComponents from '../../components/pages/About/AboutComponents'
 import AboutHeaderBanner from '../../components/pages/About/AboutHeaderBanner'
 import AnimatetdBanner from '../../components/pages/About/AnimatetdBanner'
@@ -17,6 +19,8 @@ import NavBar from '../../components/pages/Shared/NavBar'
       <Anexecutive />
       <AnimatetdBanner />
       <LoveIsFeath />
+      <Membership />
+      <MeetCard />
     </div>
   )
 }
