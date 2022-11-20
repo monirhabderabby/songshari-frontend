d7be8db866672d42f31d6e1cd1823106d5b8aa8a
developer-sahadat <mdridoysikder862@gmail.com>
2022-11-21 01:04:10 +0600

    home page completed

--- src/Wrapper/Home/Homepage.js
+++ src/Wrapper/Home/Homepage.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Anexecutive from "../../components/CardComopents/Anexecutive";
+import Membership from "../../components/membership/Membership";
 import Banner from "../../components/pages/Home/Banner/Banner";
 import ThreeEasySteps from "../../components/pages/Home/Banner/ThreeEasySteps";
 import BeginFamilyJourney from "../../components/pages/Home/BeginFamilyJourney/BeginFamilyJourney";
 import FindSoleMate from "../../components/pages/Home/FindSoleMate/FindSoleMate";
 import SecureVerified from "../../components/pages/Home/SecureVerified/SecureVerified";
 import LatestRegisteredMember from "../../components/pages/LatestRegisteredMember/LatestRegisteredMember";
+import MeetNewPeople from "../../components/pages/MeetNewPeople/MeetNewPeople";
 import PeopleJoinedAlready from "../../components/pages/PeopleJoinedAlready/PeopleJoinedAlready";
 import NavBar from "../../components/pages/Shared/NavBar";
 import TopProfession from "../../components/pages/TopProfession/TopProfession";
@@ -13,32 +16,9 @@ import Footer from "../../components/shared/Footer/Footer";
 // import NavBar from "../../components/shared/NavBar/NavBar";
 
 const Homepage = () => {
-  // <<<<<<< HEAD
-  //   return (
-  //     <div className="font-george">
-  //       <NavBar></NavBar>
-  //       <Banner></Banner>
-
-  //       <div>
-  //         <ThreeEasySteps></ThreeEasySteps>
-  //       </div>
-  //       <SecureVerified></SecureVerified>
-  //       <BeginFamilyJourney></BeginFamilyJourney>
-  //       <LatestRegisteredMember />
-  //       <TopProfile />
-  //       <PeopleJoinedAlready />
-  //       <FindSoleMate />
-  //       <Anexecutive />
-  //       <Membership />
-  //       <MeetCard />
-  //       <Footer />
-  //     </div>
-  //   );
-  // =======
   return (
     <div className="font-george">
       <NavBar></NavBar>
-
       <Banner></Banner>
       <ThreeEasySteps></ThreeEasySteps>
       <SecureVerified></SecureVerified>
@@ -48,6 +28,9 @@ const Homepage = () => {
       <PeopleJoinedAlready />
       <TopProfession />
       <FindSoleMate />
+      <MeetNewPeople />
+      <Membership />
+      <Anexecutive></Anexecutive>
       <Footer />
     </div>
   );
