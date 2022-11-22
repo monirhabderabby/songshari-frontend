6a4124661377d52c9ef6ef4e46a438f65e57f1b5
developer-sahadat <mdridoysikder862@gmail.com>
2022-11-22 14:05:25 +0600

    Lawyer Service Details completed

--- src/route/Index.js
+++ src/route/Index.js
@@ -10,6 +10,8 @@ import RegAsProfessional from "../components/pages/Authentications/RegAsProfessi
 import SignUp from "../components/pages/Authentications/SignUp";
 import Contact from "../components/pages/Contact/Contact";
 import { DynamicProfilePage } from "../components/pages/DynamicProfilePage/DynamicProfilePage";
+import LawyerProfilePage from "../components/pages/lawyerProfile/LawyerProfilePage";
+import LawyerServiceDetails from "../components/pages/LawyerServiceDetails/LawyerServiceDetails";
 import { Members } from "../components/pages/Members/Members";
 import NotFound from "../components/pages/notfound/NotFound";
 import { SingleRequestView } from "../components/pages/SingleRequestView/SingleRequestView";
@@ -41,19 +43,42 @@ const Index = () => {
           <Route path="/about" element={<About></About>}></Route>
           <Route path="/userprofile" element={<SingleProfile />}></Route>
           <Route path="/membership" element={<MemberShip></MemberShip>}></Route>
-          <Route path="/profileinfo" element={<ProfileInfo></ProfileInfo>}></Route>
+          <Route
+            path="/profileinfo"
+            element={<ProfileInfo></ProfileInfo>}
+          ></Route>
           <Route path="/course" element={<Course></Course>}></Route>
-          <Route path="/findalawyer" element={<FindAlawyer></FindAlawyer>}></Route>
+          <Route
+            path="/findalawyer"
+            element={<FindAlawyer></FindAlawyer>}
+          ></Route>
+          <Route
+            path="/lawyer-profilePage "
+            element={<LawyerProfilePage />}
+          ></Route>
+          <Route
+            path="/lawyer-service-detail"
+            element={<LawyerServiceDetails />}
+          ></Route>
           <Route path="/kazi" element={<Kazi></Kazi>}></Route>
           <Route path="/agent" element={<Agent></Agent>}></Route>
           <Route path="/community" element={<Community></Community>}></Route>
           <Route path="/shop" element={<Shop></Shop>}></Route>
-          <Route path="/stepher" element={<StepProgress></StepProgress>}></Route>
-          <Route path="/singleproduct" element={<SingleProduct></SingleProduct>}></Route>
+          <Route
+            path="/stepher"
+            element={<StepProgress></StepProgress>}
+          ></Route>
+          <Route
+            path="/singleproduct"
+            element={<SingleProduct></SingleProduct>}
+          ></Route>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/regAsProfessional" element={<RegAsProfessional />} />
-          <Route path="/loginAsProfessional" element={<LoginAsProfessional />} />
+          <Route
+            path="/loginAsProfessional"
+            element={<LoginAsProfessional />}
+          />
           <Route path="/contact" element={<Contact />}></Route>
           <Route path="/members" element={<Members />}></Route>
           <Route path="/profile/:id" element={<DynamicProfilePage />}></Route>
@@ -66,8 +91,14 @@ const Index = () => {
             <Route path="/" element={<Suggested />} />
             <Route path="/top-gride" element={<TopBride />} />
             <Route path="/top-groom" element={<TopGroom />} />
-            <Route path="/top-profession/kazi" element={<TopProfessionKazi />} />
-            <Route path="/top-profession/agent" element={<TopProfessionAgent />} />
+            <Route
+              path="/top-profession/kazi"
+              element={<TopProfessionKazi />}
+            />
+            <Route
+              path="/top-profession/agent"
+              element={<TopProfessionAgent />}
+            />
           </Route>
           <Route path="/singleViewRequest" element={<SingleRequestView />} />
           <Route path="/admin-analytics" element={<AdminAnalytics />}></Route>
