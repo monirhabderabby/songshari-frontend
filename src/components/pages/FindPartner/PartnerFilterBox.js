6c855cd98af216f04d400d72c9009ce19248fb19
developer-sahadat <mdridoysikder862@gmail.com>
2022-11-23 10:06:04 +0600

    partner fitlter box updated

--- src/components/pages/FindPartner/PartnerFilterBox.js
+++ src/components/pages/FindPartner/PartnerFilterBox.js
@@ -6,59 +6,129 @@ const PartnerFilterBox = () => {
     <>
       <div
         id="join-now-box2"
-        className="bg-[#fff] rounded-[10px] pb-[15px]"
+        className="bg-[#fff] rounded-[10px] px-[30px] py-[40px]"
         style={{
           visibility: "visible",
-          boxShadow: "0 3px 10px 0 rgb(0 0 0 / 10%)",
+          boxShadow: " 2px 2px 10px rgba(0, 0, 0, 0.12)",
         }}
       >
-        <div
-          className="flex justify-center items-center py-[10px] px-[15px] border-b-[1px] border-b-[#0000001a] "
-          style={{
-            background: "linear-gradient(145deg,#511be7,#e41272)",
-            borderTopLeftRadius: "15px",
-            borderTopRightRadius: "15px",
-          }}
-        >
-          <div className="flex items-center p-[4px]">
-            <div className="flex items-center">
-              <input
-                className="bg-white border-[1px] py-1 border-[#0000001a] rounded-[50px] "
-                type="text"
-                name="location"
-              />
-            </div>
-            <p className={` text-white ml-2  ${partnerCSS.searchFind}`}>
-              Search
-            </p>
-          </div>
-        </div>
         {/* Category input  */}
-        <div className="flex flex-col border-b-[1px] border-b-[#0000001a] py-[10px] px-[15px]">
-          <p className={`mb-2 ${partnerCSS.searchFind}`}>Category</p>
-          <div
-            id="option2"
-            className={`flex items-start flex-col ${partnerCSS.option}`}
-          >
-            <div id="s-input2">
-              <input type="checkbox" name="gender2" id="Personal" />
-              <label for="Personal">Personal Injury Lawyer</label>
-            </div>
-            <div id="s-input2">
-              <input type="checkbox" name="gender2" id="Family" />
-              <label for="Family">Family Lawyer</label>
-            </div>
-            <div id="s-input2">
-              <input type="checkbox" name="gender2" id="Estate" />
-              <label for="Estate">Estate Planning Lawyer</label>
-            </div>
-            <div id="s-input2">
-              <input type="checkbox" name="gender2" id="Contract" />
-              <label for="Contract">Contract Lawyer</label>
+        <div className="flex ] flex-col border-b-[2px] border-b-[#BDCADB] py-[10px]">
+          <h2 className="text-[#000000] text-lg font-bold mb-[16px]">
+            Category
+          </h2>
+          <div className="flex pb-[10px] justify-between items-center">
+            <h4 className="text-[#000000] font-medium text-base">
+              Personal Injury Lawyer
+            </h4>
+            <div>
+              <i class="fa-solid font-semibold fa-chevron-down"></i>
             </div>
           </div>
         </div>
         {/* location input  */}
+        <div className="flex ] mt-[20px] flex-col border-b-[1px] border-b-[#BDCADB] py-[10px]">
+          <h2 className="text-[#000000] text-lg font-bold mb-[16px]">
+            location
+          </h2>
+          <div className="flex pb-[10px] justify-between items-center">
+            <h4 className="text-[#000000] font-medium text-base">Dhaka</h4>
+            <div>
+              <i class="fa-solid font-semibold fa-chevron-down"></i>
+            </div>
+          </div>
+        </div>
+        {/* Rating input  */}
+        <div className="flex ] mt-[20px] flex-col border-b-[1px] border-b-[#BDCADB] py-[10px]">
+          <h2 className="text-[#000000] text-lg font-bold mb-[16px]">Rating</h2>
+          <div className=" pb-[10px] ">
+            <div className="flex">
+              {" "}
+              <button
+                className="w-[70px] h-[40px] py-[8px] px-[10px] rounded-xl border border-[#BDCADB] text-sm text-[#FFFFFF] font-medium"
+                style={{
+                  background:
+                    " linear-gradient(180deg, #E41272 0%, #942DD9 100%)",
+                }}
+              >
+                1 Start
+              </button>
+              <button className="w-[70px] h-[40px] py-[8px] px-[10px] ml-[28px] rounded-xl border border-[#BDCADB] text-sm text-[#6E84AB] font-medium">
+                2 Start
+              </button>
+            </div>
+            <div className="flex mt-[16px]">
+              {" "}
+              <button className="w-[70px] h-[40px] py-[8px] px-[10px]  rounded-xl border border-[#BDCADB] text-sm text-[#6E84AB] font-medium">
+                3 Start
+              </button>
+              <button className="w-[70px] h-[40px] py-[8px] px-[10px] ml-[28px] rounded-xl border border-[#BDCADB] text-sm text-[#6E84AB] font-medium">
+                4 Start
+              </button>
+            </div>
+            <div className="flex mt-[16px]">
+              {" "}
+              <button className="w-[70px] h-[40px] py-[8px] px-[10px]  rounded-xl border border-[#BDCADB] text-sm text-[#6E84AB] font-medium">
+                5 Start
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/*  Remuneration box  */}
+        <div className="flex ] mt-[20px] flex-col border-b-[1px] border-b-[#BDCADB] py-[10px]">
+          <h2 className="text-[#000000] text-lg font-bold mb-[16px]">
+            Remuneration
+          </h2>
+          <div className="flex pb-[10px] justify-between items-center">
+            <h4 className="text-[#000000] font-medium text-base">
+              30,000 - 40,000
+            </h4>
+            <div>
+              <i class="fa-solid font-semibold fa-chevron-down"></i>
+            </div>
+          </div>
+        </div>
+        {/*  Total Case */}
+        <div className="flex ] mt-[20px] flex-col border-b-[1px] border-b-[#BDCADB] py-[10px]">
+          <h2 className="text-[#000000] text-lg font-bold mb-[16px]">
+            Total Case
+          </h2>
+          <div className="flex pb-[10px] justify-between items-center">
+            <h4 className="text-[#000000] font-medium text-base">25+</h4>
+            <div>
+              <i class="fa-solid font-semibold fa-chevron-down"></i>
+            </div>
+          </div>
+        </div>
+        {/*  Qualifications*/}
+        <div className="flex ] mt-[20px] flex-col border-b-[1px] border-b-[#BDCADB] py-[10px]">
+          <h2 className="text-[#000000] text-lg font-bold mb-[16px]">
+            Qualifications
+          </h2>
+          <div className="flex pb-[10px] justify-between items-center">
+            <h4 className="text-[#000000] font-medium text-base">
+              Bankruptcy Lawyer
+            </h4>
+            <div>
+              <i class="fa-solid font-semibold fa-chevron-down"></i>
+            </div>
+          </div>
+        </div>
+
+        {/* submit button  */}
+        <div className="flex justify-center items-center ] mt-[30px] ">
+          <button
+            style={{
+              background: "linear-gradient(180deg, #E41272 0%, #942DD9 100%)",
+            }}
+            className="w-[179px] text-center py-[8] px-[10px] text-[#fff] h-[54px] text-2xl font-medium rounded-xl"
+          >
+            Submit
+          </button>
+        </div>
+
+        {/* location input 
         <div className="flex flex-col border-b-[1px] border-b-[#0000001a] py-[10px] px-[15px]">
           <p className={`mb-2 ${partnerCSS.searchFind}`}>Location</p>
           <div className={`flex items-start flex-col ${partnerCSS.option}`}>
@@ -79,9 +149,9 @@ const PartnerFilterBox = () => {
               <label for="Rajshahi">Rajshahi</label>
             </div>
           </div>
-        </div>
+        </div> */}
         {/* Remuneration box */}
-        <div className="flex flex-col border-b-[1px] border-b-[#0000001a] py-[10px] px-[15px]">
+        {/* <div className="flex flex-col border-b-[1px] border-b-[#0000001a] py-[10px] px-[15px]">
           <p className={`mb-2 ${partnerCSS.searchFind}`}>Remuneration</p>
           <div className={`flex items-center  ${partnerCSS.option}`}>
             <div className=" mr-2">
@@ -100,9 +170,9 @@ const PartnerFilterBox = () => {
               </select>
             </div>
           </div>
-        </div>
+        </div> */}
         {/* rating box  */}
-        <div className="flex flex-col border-b-[1px] border-b-[#0000001a] py-[10px] px-[15px]">
+        {/* <div className="flex flex-col border-b-[1px] border-b-[#0000001a] py-[10px] px-[15px]">
           <p className={`mb-2 ${partnerCSS.searchFind}`}>Rating</p>
           <div className={`flex items-start flex-col ${partnerCSS.option}`}>
             <div id="s-input2" className="s-input">
@@ -126,9 +196,9 @@ const PartnerFilterBox = () => {
               <label for="star1">1 Star</label>
             </div>
           </div>
-        </div>
+        </div> */}
         {/* Qualifications box */}
-        <div className="flex flex-col border-b-[1px] border-b-[#0000001a] py-[10px] px-[15px]">
+        {/* <div className="flex flex-col border-b-[1px] border-b-[#0000001a] py-[10px] px-[15px]">
           <p className={`mb-2 ${partnerCSS.searchFind}`}>Qualifications</p>
           <div className={`flex items-start flex-col ${partnerCSS.option}`}>
             <div id="s-input2" className="s-input">
@@ -148,15 +218,15 @@ const PartnerFilterBox = () => {
               <label for="Tax">Tax Lawyer</label>
             </div>
           </div>
-        </div>
-        <div className="px-[15px] py-[10px]">
+        </div> */}
+        {/* <div className="px-[15px] py-[10px]">
           <p className={`mb-2 w-full ${partnerCSS.searchFind}`}>
             Total Case <span className="ml-1">(+25)</span>
           </p>
         </div>
         <div className="text-center">
           <button className={partnerCSS.partnerButton}>Submit</button>
-        </div>
+        </div> */}
       </div>
     </>
   );
