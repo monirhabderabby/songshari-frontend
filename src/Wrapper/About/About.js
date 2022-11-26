ef883dc2439295c71325fd5274285fa86499eb1d
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-11-26 11:24:22 +0600

    about us for pc completed

--- src/Wrapper/About/About.js
+++ src/Wrapper/About/About.js
@@ -5,9 +5,16 @@ import CommonBanner from "../../components/comons/CommonBanner";
 import Membership from "../../components/membership/Membership";
 import AboutComponents from "../../components/pages/About/AboutComponents";
 import AboutHeaderBanner from "../../components/pages/About/AboutHeaderBanner";
+import AboutOurMission from "../../components/pages/About/AboutOurMission";
+import AboutUsBlog from "../../components/pages/About/AboutUsBlog";
+import AboutUsCommunity from "../../components/pages/About/AboutUsCommunity";
+import AboutUsMotive from "../../components/pages/About/AboutUsMotive";
+import AboutUsOurHistory from "../../components/pages/About/AboutUsOurHistory";
+import AboutUsStory from "../../components/pages/About/AboutUsStory";
 import AnimatetdBanner from "../../components/pages/About/AnimatetdBanner";
 import LoveIsFeath from "../../components/pages/About/LoveIsFeath";
 import NavBar from "../../components/pages/Shared/NavBar";
+import CustomHeader from "../../components/shared/CustomHeader/CustomHeader";
 import Footer from "../../components/shared/Footer/Footer";
 
 const About = () => {
@@ -17,16 +24,23 @@ const About = () => {
 
     return (
         <div>
-            <NavBar />
-            <CommonBanner title="About Us" currentStage="> About" />
+            <CustomHeader title={"About Us"} />
+            {/* <NavBar /> */}
+            {/* <CommonBanner title="About Us" currentStage="> About" /> */}
             <AboutHeaderBanner />
-            <AboutComponents />
-            <Anexecutive />
-            <AnimatetdBanner />
-            <LoveIsFeath />
-            <Membership />
-            <Anexecutive />
-            <MeetCard />
+            <AboutOurMission />
+            <AboutUsStory />
+            <AboutUsOurHistory />
+            <AboutUsBlog />
+            <AboutUsCommunity />
+            <AboutUsMotive />
+            {/* <AboutComponents /> */}
+            {/* <Anexecutive /> */}
+            {/* <AnimatetdBanner /> */}
+            {/* <LoveIsFeath /> */}
+            {/* <Membership /> */}
+            {/* <Anexecutive /> */}
+            {/* <MeetCard /> */}
             <Footer />
         </div>
     );
---
+++ src/components/pages/About/AboutUsStory.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import aboutUsStoryImg from "../../../assets/images/AboutUs/aboutUsStory.jpg";
+
+const AboutUsStory = () => {
+    return (
+        <div>
+            <h1 className='text-3xl font-semibold text-center lead-[44px] mt-20 mb-12'>
+                Our Story
+            </h1>
+            <div className='max-w-5xl mx-auto flex items-start gap-16'>
+                <div className=''>
+                    <img className='w-[584px] rounded-[30px]' src={aboutUsStoryImg} alt="" />
+                </div>
+                <div className='w-96 text-[#757575] leading-7'>
+                    <p className='mb-4'>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto iure accusamus, recusandae nisi quod laudantium reprehenderit. Excepturi itaque adipisci, rem eligendi corporis eum dolorem eius harum perspiciatis pariatur perferendis dignissimos ducimus quaerat et ad cumque ratione accusamus dolorum debitis saepe iure earum molestias obcaecati. Ab, vitae.
+                    </p>
+                    <p className='mb-4'>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto iure accusamus, recusandae nisi quod laudantium reprehenderit. Excepturi itaque adipisci, rem eligendi corporis eum dolorem eius harum perspiciatis pariatur perferendis dignissimos ducimus quaerat et ad cumque ratione accusamus dolorum debitis saepe iure earum molestias obcaecati. Ab, vitae.
+                    </p>
+                    <p className="mb-4">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis voluptas possimus expedita, ullam est doloribus fugit inventore ipsam harum ad!
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AboutUsStory;
