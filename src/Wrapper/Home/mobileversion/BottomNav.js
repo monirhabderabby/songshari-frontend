51c33c0a4e99b59e97c27d533907f0213d87162a
AnikHaque <ekramul15-811@diu.edu.bd>
2022-11-27 21:26:24 +0600

    updated

--- src/Wrapper/Home/mobileversion/BottomNav.js
+++ src/Wrapper/Home/mobileversion/BottomNav.js
@@ -14,11 +14,11 @@ export const BottomNav = () => {
   return (
     <div className='rounded-full bg-bottom-nav'>
         <div className='grid grid-cols-5 gap-4 bottom-bar'>
-<img src={member} className='w-10'></img>
-<img src={Shop} className='w-10'></img>
-<img src='https://i.ibb.co/d5yyNd6/512-512-Icon.png' className='w-10'></img>
-<img src={cart} className='w-10'></img>
-<Link to='/setting'><img src={cart} className='w-10'></img></Link>
+<img src={member} className='w-8'></img>
+<img src={Shop} className='w-8'></img>
+<img src='https://i.ibb.co/d5yyNd6/512-512-Icon.png' className='w-8'></img>
+<img src={cart} className='w-8'></img>
+<Link to='/setting'><img src={cart} className='w-8'></img></Link>
 
 
 
