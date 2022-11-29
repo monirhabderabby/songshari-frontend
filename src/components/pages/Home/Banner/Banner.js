4d3885e4d13de2a77e631ef37744e5fc26072b38
Monir <monirhrabby.personal@gmail.com>
2022-11-29 07:45:23 +0600

    modify banner

--- src/components/pages/Home/Banner/Banner.js
+++ src/components/pages/Home/Banner/Banner.js
@@ -1,32 +1,13 @@
 import { Fragment } from "react";
-import { Typewriter } from "react-simple-typewriter";
 import banner from "../../../../assets/images/banner/banner.png";
-import "./Banner.css";
 import BannerFilterBox from "./BannerFilterBox";
 import BannerProfile from "./BannerProfile";
 
 const Banner = () => {
     return (
         <Fragment>
-            <section className="banner">
+            <section className="bg-[url('https://img1.picmix.com/output/stamp/normal/9/9/4/3/1503499_8efaf.gif')] h-[calc(100vh-90px)] flex items-center">
                 <div className="custom-container py-[50px]">
-                    <h2
-                        // style={{ textShadow: " 2px 2px 8px #7B047D" }}
-                        className="text-center mb-[43px] text-[74px] font-bold leading-[94px] text-[#000000]"
-                    >
-                        Starting Your
-                        <span className="text-[#701FD8] ml-[5px]">
-                            <Typewriter
-                                words={["Family Journey", "New Journey", "Life Journey"]}
-                                loop={3}
-                                typeSpeed={70}
-                                deleteSpeed={50}
-                                delaySpeed={2000}
-                            />
-                        </span>{" "}
-                        Is
-                        <br /> So Easy With <span className="text-[#E41272]">Shongshari.Com</span>
-                    </h2>
                     <div className="grid grid-cols-4 gap-x-2 justify-center items-center">
                         <div>
                             <BannerFilterBox />
