09ac709308c0586a153925f43b2da58c1c27f3fe
Monir <monirhrabby.personal@gmail.com>
2022-11-29 11:45:12 +0600

    modified about us page

--- src/Wrapper/About/About.js
+++ src/Wrapper/About/About.js
@@ -1,14 +1,8 @@
 import React, { useEffect } from "react";
 import AboutHeaderBanner from "../../components/pages/About/AboutHeaderBanner";
 import AboutOurMission from "../../components/pages/About/AboutOurMission";
-import AboutUsBlog from "../../components/pages/About/AboutUsBlog";
-import AboutUsCommunity from "../../components/pages/About/AboutUsCommunity";
-import AboutUsMotive from "../../components/pages/About/AboutUsMotive";
 import AboutUsOurHistory from "../../components/pages/About/AboutUsOurHistory";
 import AboutUsStory from "../../components/pages/About/AboutUsStory";
-import MobileDiamondPlan from "../../components/pages/Membership/MobileVersion/MobileDiamondPlan";
-import MobileFreePlan from "../../components/pages/Membership/MobileVersion/MobileFreePlan";
-import MobileGoldPlan from "../../components/pages/Membership/MobileVersion/MobileGoldPlan";
 import MobilePlatinumPlan from "../../components/pages/Membership/MobileVersion/MobilePlatinumPlan";
 import CustomHeader from "../../components/shared/CustomHeader/CustomHeader";
 import Footer from "../../components/shared/Footer/Footer";
@@ -26,9 +20,6 @@ const About = () => {
                 <AboutOurMission />
                 <AboutUsStory />
                 <AboutUsOurHistory />
-                <AboutUsBlog />
-                <AboutUsCommunity />
-                <AboutUsMotive />
                 <Footer />
             </div>
             <div className="md:hidden">
