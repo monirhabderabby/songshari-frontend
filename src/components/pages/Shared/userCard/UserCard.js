d8655dc3037f03b8b6042dfb61625164849516c8
Monir <monirhrabby.personal@gmail.com>
2022-12-01 18:43:30 +0600

    modify profile card

--- src/components/pages/Shared/userCard/UserCard.js
+++ src/components/pages/Shared/userCard/UserCard.js
@@ -11,9 +11,13 @@ export const UserCard = ({ profile, isLoading }) => {
     return (
         <div className="col-span-1" data-aos="fade-up" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
             {profile && (
-                <div className="h-[370px] relative group overflow-hidden Suggested-card">
-                    <img className="h-[370px] w-full" src={profile?.profilePhoto} alt="profile" />
-                    <div className="absolute h-[100px] w-full top-[270px] right-0 inset-0 bg-[linear-gradient(166deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)] opacity-90 text-white px-4 py-2 font-serif group-hover:top-[370px] transition ease-out duration-500">
+                <div className="h-[400px] relative group overflow-hidden Suggested-card">
+                    <img
+                        className="h-[400px] rounded-tr-[12px] rounded-tl-[12px] rounded-bl-[12px] rounded-br-[12px] object-cover w-full"
+                        src={profile?.profilePhoto}
+                        alt="profile"
+                    />
+                    <div className="absolute h-[100px] w-full top-[300px] right-0 inset-0 bg-[linear-gradient(166deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)] opacity-90 text-white px-4 py-2 font-serif group-hover:top-[370px] transition ease-out duration-500 rounded-bl-[12px] rounded-br-[12px]">
                         <div className="flex gap-x-4 items-center">
                             <p className="text-[20px] text-white font-bold drop-shadow-lg">{profile?.firstName + " " + profile?.lastName}</p>
                             <p className="text-[18px] text-[#87ceeb]">{ageCalculator(profile?.dateOfBirth ? profile?.dateOfBirth : 0)}</p>
@@ -21,14 +25,6 @@ export const UserCard = ({ profile, isLoading }) => {
                         <p>{profile?.designation ? profile?.designation : "Not Provided"}</p>
                         <p>{profile?.hometown ? profile?.hometown : "Not Provided"}</p>
                     </div>
-                    {/* <div className="px-[15px]  py-[10px] text-white bg-[linear-gradient(166deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)]">
-                        <div className="flex gap-x-5 items-center">
-                            <p className="text-[22px]">{profile?.firstName + " " + profile?.lastName}</p>
-                            <p className="text-[18px]">{ageCalculator(profile?.dateOfBirth ? profile?.dateOfBirth : 0)}</p>
-                        </div>
-                        <p>{profile?.designation ? profile?.designation : "Not Provided"}</p>
-                        <p>{profile?.hometown ? profile?.hometown : "Not Provided"}</p>
-                    </div> */}
                     <div
                         style={{
                             backgroundImage: `url(${bg})`,
@@ -47,26 +43,19 @@ export const UserCard = ({ profile, isLoading }) => {
                             alt="profile"
                             className="w-[94px] h-[94px] rounded-full relative left-[35%] border-white border-4"
                         />
-                        <div className="p-2">
+                        <div className="p-2 relative">
                             <h4 className="text-white text-[25px] font-bold text-center overflow-hidden">
                                 <span className="font-george">Monir Hossain</span>
                                 <span style={{ color: "rgb(135, 206, 235)" }}> 27</span>
                             </h4>
-                            <ul className="text-white font-george">
+                            <ul className="text-white font-Inter">
                                 <li>
-                                    <p className="text-[14px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti veritatis nobis</p>
-                                </li>
-                                <li>
-                                    <p>
-                                        <span>Education</span>{" "}
-                                        {profile?.hightestEducationalQualification
-                                            ? profile?.hightestEducationalQualification?.degreeName +
-                                              `(Department Of ${profile?.hightestEducationalQualification?.Department})`
-                                            : "Not Provided"}
+                                    <p className="text-[14px] font-normal">
+                                        Lorem ipsum dolor sit amet gfdgfgd sdfdsfs consectetur adipisicing elit. Minus dolor dolore sint ea. Non,
                                     </p>
                                 </li>
                             </ul>
-                            <div className="h-[30px] w-full flex items-center justify-evenly">
+                            <div className="h-[30px] w-full flex items-center justify-evenly absolute -bottom-[40px]">
                                 <p
                                     onClick={() => navigate(`/profile/${profile?._id}`)}
                                     className="bg-[linear-gradient(166deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)] rounded-full cursor-pointer"
