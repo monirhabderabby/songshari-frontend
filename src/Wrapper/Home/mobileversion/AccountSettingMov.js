16d7ab477269d2df95d12b03c26b5c67e6f5e627
AnikHaque <ekramul15-811@diu.edu.bd>
2022-12-03 21:20:32 +0600

    updated

--- src/Wrapper/Home/mobileversion/AccountSettingMov.js
+++ src/Wrapper/Home/mobileversion/AccountSettingMov.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Box from '@mui/material/Box';
+import Popper from '@mui/material/Popper';
 
 export const AccountSettingMov = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
+  const handleClick = (event) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popper' : undefined;
   return (
 
     <div className='mt-10'>
@@ -169,9 +178,25 @@ export const AccountSettingMov = () => {
 
         </div>
       </Link>
+     
 
-
-
+      <button aria-describedby={id} type="button" onClick={handleClick} className='ml-20 mt-4'>
+        About Shongshari
+      </button>
+      <Popper id={id} open={open} anchorEl={anchorEl}>
+        <Box sx={{ border: 1, p: 1, bgcolor: 'background.paper' }} className='p-12'>
+          <Link to='/meetmov'>Team Member</Link>
+          <br></br>
+          <Link to='/privacymov'>Privacy Policy</Link>
+          <br></br>
+          <Link to='/aboutmov'>About Us</Link>
+          <br></br>
+          <Link to='/historymov'>History</Link>
+          <br></br>
+          <Link to='/communitymov'>Community</Link>
+         
+        </Box>
+      </Popper>
     </div>
   )
 }
