3a9ac81a0f01fe8fd5e0e0093eac567a0cea5824
Monir <monirhrabby.personal@gmail.com>
2022-12-03 21:38:19 +0600

    added code

--- src/components/SingleProfilesUser/FriendsSection/Friends.js
+++ src/components/SingleProfilesUser/FriendsSection/Friends.js
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import "../../../App.css";
 
 import { useGetAllRequestQuery } from "../../../Redux/features/connections/connectionApi";
+import { AllFriendRequest } from "./AllFriendRequest";
 import { AllFriends } from "./AllFriends";
+import { AllSentRequest } from "./AllSentRequest";
 
 export const Friends = ({ data, isLoading }) => {
     const [page, setPage] = useState(2);
@@ -14,22 +16,49 @@ export const Friends = ({ data, isLoading }) => {
     return (
         <>
             <div className="mb-4 w-full flex justify-around items-center">
-                <button className="py-[12px] px-[20px] text-[#333333] border-[1px] border-[#333333] rounded-[10px] text-[16px] leading-[26px] font-Inter font-medium">
+                <button
+                    className={`h-[60px] w-[110px] ${
+                        page === 1
+                            ? "text-white shadow-[2px_2px_12px_rgba(0,0,0,0.12)] bg-[linear-gradient(180deg,_#EA5FA4_0%,_#A52BC7_100%)]"
+                            : "text-[#333333] border-[1px] border-[#333333]"
+                    }  rounded-[10px] text-[16px] leading-[26px] font-Inter font-medium`}
+                    onClick={() => setPage(1)}
+                >
                     Friends
                 </button>
-                <button className="py-[12px] px-[20px] rounded-[10px] shadow-[2px_2px_12px_rgba(0,0,0,0.12)] bg-[linear-gradient(180deg,_#EA5FA4_0%,_#A52BC7_100%)] text-[16px] leading-[26px] font-Inter font-medium  text-white ">
+                <button
+                    className={`h-[60px] w-[177px] rounded-[10px] ${
+                        page === 2
+                            ? "shadow-[2px_2px_12px_rgba(0,0,0,0.12)] bg-[linear-gradient(180deg,_#EA5FA4_0%,_#A52BC7_100%)] text-white"
+                            : "text-[#333333] border-[1px] border-[#333333]"
+                    } text-[16px] leading-[26px] font-Inter font-medium `}
+                    onClick={() => setPage(2)}
+                >
                     <div className="relative">
                         Friend Request{" "}
-                        <span className="absolute -top-2 -right-4 bg-white text-[#333333] rounded-full text-[10px] w-[18px] h-[18px] flex items-center justify-center">
+                        <span
+                            className={`absolute -top-2 right-3 ${
+                                page === 2 ? "bg-white text-[#333333]" : "bg-[linear-gradient(180deg,_#EA5FA4_0%,_#A52BC7_100%)] text-white"
+                            } rounded-full text-[10px] w-[18px] h-[18px] flex items-center justify-center`}
+                        >
                             10
                         </span>
                     </div>
                 </button>
-                <button className="py-[12px] px-[20px] text-[#333333] border-[1px] border-[#333333] rounded-[10px] text-[16px] leading-[26px] font-Inter font-medium">
+                <button
+                    className={`h-[60px] w-[164px] ${
+                        page === 3
+                            ? "text-white shadow-[2px_2px_12px_rgba(0,0,0,0.12)] bg-[linear-gradient(180deg,_#EA5FA4_0%,_#A52BC7_100%)]"
+                            : "text-[#333333] border-[1px] border-[#333333]"
+                    }  rounded-[10px] text-[16px] leading-[26px] font-Inter font-medium`}
+                    onClick={() => setPage(3)}
+                >
                     Request Sent List
                 </button>
             </div>
-            {page === 2 && <AllFriends />}
+            {page === 1 && <AllFriends />}
+            {page === 2 && <AllFriendRequest />}
+            {page === 3 && <AllSentRequest />}
         </>
     );
 };
