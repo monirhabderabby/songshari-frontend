7ee078210ac9c25424f31e7c61ddf12fdca4c82c
Md-Tur <turibnmostafiz@gmail.com>
2022-12-10 19:16:23 +0600

    Find partner page warning removed

--- src/components/pages/FindPartner/FindAPartnerSuggested.js
+++ src/components/pages/FindPartner/FindAPartnerSuggested.js
@@ -5,15 +5,12 @@ import { UserCard } from "../Shared/userCard/UserCard";
 export const FindAPartnerSuggested = () => {
     const { data, isSuccess, isLoading } = useGetRecentMembersQuery();
 
-    if (isSuccess) {
-        console.log(data);
-    }
     return (
         <div className="mt-[30px] max-w-[950px] mx-auto">
             <div className="grid grid-cols-3 gap-[30px]">
                 {isSuccess &&
                     data?.map(profile => {
-                        return <UserCard {...{ profile }} />;
+                        return <UserCard key={profile._id} {...{ profile }} />;
                     })}
                 {isLoading && <UserCard {...{ isLoading }} />}
             </div>
