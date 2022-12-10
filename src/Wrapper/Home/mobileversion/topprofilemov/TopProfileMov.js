a33c1d71609409a5cf216e812ab061b659f6e7c7
AnikHaque <ekramul15-811@diu.edu.bd>
2022-12-10 13:53:39 +0600

    added

--- src/Wrapper/Home/mobileversion/topprofilemov/TopProfileMov.js
+++ src/Wrapper/Home/mobileversion/topprofilemov/TopProfileMov.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { Link, NavLink, Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 
 const TopProfileMov = () => {
   return (
     <div className="bg-[#F8F8FF]">
      <div className="mt-8 pt-8">
-        <Link to="" className='bg-home-suggested pr-4 pt-2 pb-2 pl-2 ml-2 mr-4 text-white'> Suggested For You </Link>
-        <Link to="/" className='pl-4 pr-4 pt-2 pb-2 mr-4  bg-gome-2 '> Top Profile </Link>
-        <Link to="/" className='pl-4 pr-4 pt-2 pb-2  bg-gome-3 '> Top Groom </Link>
+        <Link to="" className='bg-home-suggested pr-4 pt-2 pb-2 pl-2 ml-2  text-white'> Suggested For You </Link>
+        <Link to="/" className='pl-4  pt-2 pb-2   bg-gome-2 '> Top Profile </Link>
+        <Link to="/" className='pl-4  pt-2 pb-2  bg-gome-3 '> Top Groom </Link>
       </div>
       <Outlet></Outlet>
     </div>
--- src/components/SingleProfilesUser/SingleUserActivity/SingleUserActivity.js
+++ src/components/SingleProfilesUser/SingleUserActivity/SingleUserActivity.js
@@ -6,6 +6,12 @@ import { MdCreateNewFolder } from "react-icons/md";
 import { firebaseStorage } from '../../../firebase.init';
 import { getDownloadURL, uploadBytes } from 'firebase/storage';
 import { v4 as uuidv4 } from "uuid";
+import coolicon from "../../../assets/images/activity/coolicon.png";
+import { Select } from "antd";
+import banner from "../../../assets/images/activity/postBanner.png";
+import { Link } from "react-router-dom";
+import { BottomNav } from "../../../Wrapper/Home/mobileversion/BottomNav";
+import { MovDynamicActivity } from '../../pages/DynamicProfilePage/MovDynamicActivity';
 
 const SingleUserActivity = () => {
 
@@ -46,7 +52,9 @@ const SingleUserActivity = () => {
     };
 
     return (
-        <div className="grid grid-cols-1 gap-y-[30px]">
+        <div>
+            <div className='hidden md:block'>
+            <div className="grid grid-cols-1 gap-y-[30px]">
             <div className="w-[457px] mx-auto shadow-[2px_2px_4px_rgba(0,0,0,0.12)] bg-white rounded-[10px] p-[30px] font-Inter font-normal text-[11px]">
                 <div className="flex w-full h-[100px] gap-[19px]">
                     <img className="w-12 h-10 rounded-full" src={profile} alt="profile" />
@@ -75,6 +83,66 @@ const SingleUserActivity = () => {
                 </div>
             </div>
             <DynamicActivityPage postRefetch={postRefetch}></DynamicActivityPage>
+        </div>
+            </div>
+            <div className='md:hidden w-full'>
+            <div
+          className="bg-[#FFFFFF] rounded-md pb-[5px] my-[10px]"
+          style={{ boxShadow: "2px 2px 4px rgba(0, 0, 0, 0.12)" }}
+        >
+          <div className="max-w-[338px] border-[rgba(0, 0, 0, 0.2)] pt-[30px] pb-[50px] border-b-[1px] mx-auto flex px-6">
+            <div className="relative">
+              <img
+                className="w-[40px] h-[40px] rounded-[50px]"
+                src={profile}
+                alt="Not Available"
+              />
+            </div>
+            <div className="ml-[25px]">
+            <textarea ref={ref} id="message" className='text-[#757575] w-full focus:outline-none resize-none' name='post_description' placeholder='Write somethiings here......'></textarea>
+            </div>
+          </div>
+          <div className="flex max-w-[338px] mt-[20px] mx-auto justify-between items-center px-6">
+            <div className="flex justify-between items-center">
+              <button className="bg-[#F7E9F8] flex justify-center items-center w-[51px] h-[31px] rounded-[20px]">
+                <img src={coolicon} alt="Not Available" />
+              </button>
+              <div className="ml-[20px]">
+                <Select
+                  defaultValue="Public"
+                  style={{ width: 105, borderRadius: "50px" }}
+                  options={[
+                    {
+                      value: "Public",
+                      label: "Public",
+                    },
+                    {
+                      value: "lucy",
+                      label: "Lucy",
+                    },
+                    {
+                      value: "disabled",
+                      disabled: true,
+                      label: "Disabled",
+                    },
+                    {
+                      value: "Yiminghe",
+                      label: "yiminghe",
+                    },
+                  ]}
+                />
+              </div>
+            </div>
+            <button className="border-[1px] border-[rgba(0,0,0,0.1)] rounded-[50px] py-[6px] px-5 font-bold text-[17px] text-[#FFFFFF] bg-gradient-to-t from-[#942DD9] to-[#F22876] shadow-[0.872px_9.962px_20px_rgba(12, 78, 165, 0.3)]" onClick={handleMessage}>
+                        Post
+                    </button>
+          </div>
+         
+          <MovDynamicActivity postRefetch={postRefetch}></MovDynamicActivity>
+        </div>
+            </div>
+
+        
         </div>
     );
 };
