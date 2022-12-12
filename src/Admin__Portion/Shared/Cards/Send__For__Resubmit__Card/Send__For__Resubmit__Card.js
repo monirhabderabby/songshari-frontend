a316ff427b7c4e79430498ba421898f998996e8f
Monir <monirhrabby.personal@gmail.com>
2022-12-12 16:33:39 +0600

    added send__For__Resubmit__Card

---
+++ src/Admin__Portion/Shared/Cards/Send__For__Resubmit__Card/Send__For__Resubmit__Card.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const Send__For__Resubmit__Card = () => {
+    return (
+        <div>
+            <div class="w-[385px] h-[222px] bg-[#DF933B] rounded-[8px] px-[35px] py-[31px]">
+                <div class="flex gap-x-[24px]">
+                    <img
+                        class="w-[85px] h-[85px] rounded-full"
+                        src="https://scontent.frjh5-1.fna.fbcdn.net/v/t39.30808-6/310814492_3383940818533755_1197676094327108122_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGA9lfn6WujkVz6fYFMVoFwjDMuNZrRHNWMMy41mtEc1bicZO-AvSF9FXCNTfRYimndHxJlLdYlNYLMqh4qcFXK&_nc_ohc=pnwXwFDs9p4AX84f9_g&tn=5Af7VWSQfe-WPdJS&_nc_ht=scontent.frjh5-1.fna&oh=00_AfCnC0OauOwJbL5QBOgOb6NmrEn6TxqsCJ4KvHwiuqxG9A&oe=639B55DC"
+                        alt="profile"
+                    />
+                    <div class="flex flex-col gap-y-[18px]">
+                        <h1 class="text-[#FFFFFF] text-[24px] leading-[29px] font-medium">Jhon William</h1>
+                        <p class="text-[#FFFFFF] text-[20px] leading-[24px] font-medium">Age: 26</p>
+                        <button class="w-[135px] h-[37px] mt-[20px] flex justify-center items-center bg-[linear-gradient(166deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)] text-white rounded-[4px]">
+                            View Request
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
