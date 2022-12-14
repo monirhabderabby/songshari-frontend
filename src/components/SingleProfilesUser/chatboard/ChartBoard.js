5d8903c5fb42514aa29e8d9da55c98cb9d066ab2
Md-Tur <turibnmostafiz@gmail.com>
2022-12-14 21:52:34 +0600

    user profile design fix

--- src/components/SingleProfilesUser/chatboard/ChartBoard.js
+++ src/components/SingleProfilesUser/chatboard/ChartBoard.js
@@ -51,7 +51,7 @@ const ChartBoard = () => {
                 </div>
                 <div className="grid  grid-cols-1 gap-y-[19px] gap-x-4 p-5">
                     {data
-                        ? data?.data?.suggestion.map(profile => {
+                        ? data?.data?.suggestion.slice(0, 4).map(profile => {
                             return <CardYouMayLike key={profile._id} {...{ data: profile }} />;
                         })
                         : number6.map(n => {
