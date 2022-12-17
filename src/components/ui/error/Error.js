199efc6ef30cdef6d6e16961e7e1c1da00529a88
Monir <monirhrabby.personal@gmail.com>
2022-12-17 18:27:49 +0600

    fix gender dropdown design in sign up page

--- src/components/ui/error/Error.js
+++ src/components/ui/error/Error.js
@@ -3,10 +3,10 @@ import { MdErrorOutline } from "react-icons/md";
 
 export default function Error({ message }) {
     return (
-        <div className="flex items-center">
-            <div className="relative col-span-2 mt-3 bg-red-200 max-w-xl px-3 py-2 text-red-500 rounded shadow w-full duration-500 transition-all flex items-center">
+        <div className="flex items-center w-full">
+            <div className="relative col-span-2 mt-3 bg-red-200 px-3 py-2 text-red-500 rounded shadow w-full duration-500 transition-all flex items-center">
                 <MdErrorOutline className="mr-[5px]" />
-                <span className="block text-[10px]">{message}</span>
+                <span className="block text-[11px]">{message}</span>
             </div>
         </div>
     );
