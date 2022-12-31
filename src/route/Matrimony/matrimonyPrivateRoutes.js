9a9e06aabc6a5faf5c928d907393031980b1779b
Monir <monirhrabby.personal@gmail.com>
2022-12-31 08:41:18 +0600

    set bestedRoute in edit info for User

--- src/route/Matrimony/matrimonyPrivateRoutes.js
+++ src/route/Matrimony/matrimonyPrivateRoutes.js
@@ -1,3 +1,8 @@
+import EditEducationalInfo from "../../components/SingleProfilesUser/profileSection/EditprofileSection/EditEducationalInfo";
+import EditOthersInfo from "../../components/SingleProfilesUser/profileSection/EditprofileSection/EditOthersInfo";
+import EditPersonalInfo from "../../components/SingleProfilesUser/profileSection/EditprofileSection/EditPersonalInfo";
+import EditPhysicalInfo from "../../components/SingleProfilesUser/profileSection/EditprofileSection/EditPhysicalInfo";
+import EditProfesionalInfo from "../../components/SingleProfilesUser/profileSection/EditprofileSection/EditProfesionalInfo";
 import ProfileDetails from "../../components/SingleProfilesUser/profileSection/ProfileDetails";
 import { SingleProfiles } from "../../components/SingleProfilesUser/SingleProfiles";
 import { Container } from "../../Wrapper/Stepper/Container";
@@ -16,6 +21,33 @@ const matrimonyPrivateRoutes = [
             {
                 id: 2,
                 path: "edit",
+                nestedRoutes: [
+                    {
+                        id: 1,
+                        path: "personalinfo/:id",
+                        element: <EditPersonalInfo />,
+                    },
+                    {
+                        id: 2,
+                        path: "profesionalinfo/:id",
+                        element: <EditProfesionalInfo />,
+                    },
+                    {
+                        id: 3,
+                        path: "educationalinfo/:id",
+                        element: <EditEducationalInfo />,
+                    },
+                    {
+                        id: 4,
+                        path: "physicalinfo/:id",
+                        element: <EditPhysicalInfo />,
+                    },
+                    {
+                        id: 5,
+                        path: "othersinfo/:id",
+                        element: <EditOthersInfo />,
+                    },
+                ],
             },
         ],
     },
