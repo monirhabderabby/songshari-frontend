edf8641447dbd9c2db4aaba4819edb944c690b80
Monir <monirhrabby.personal@gmail.com>
2023-01-02 18:36:08 +0600

    fix home town auto suggestion in filter

--- src/components/pages/FindPartner/AccordionPartner.js
+++ src/components/pages/FindPartner/AccordionPartner.js
@@ -32,6 +32,201 @@ export const AccordionPartner = ({ data, isLoading }) => {
     const [girlAge, setGirlAge] = useState(0);
     const [childrenLiveStatus, setChildrenLiveStatus] = useState("");
 
+    const hometown = [
+        {
+            value: "Chittagong",
+        },
+        {
+            value: "Dhaka",
+        },
+        {
+            value: "Rajshahi",
+        },
+        {
+            value: "Sylhet",
+        },
+        {
+            value: "Jessore",
+        },
+        {
+            value: "Dinajpur",
+        },
+        {
+            value: "Mymensingh",
+        },
+        {
+            value: "Comilla",
+        },
+        {
+            value: "Barisal",
+        },
+        {
+            value: "Faridpur",
+        },
+        {
+            value: "Bogra",
+        },
+        {
+            value: "Pabna",
+        },
+        {
+            value: "Rangamati",
+        },
+        {
+            value: "Kushtia",
+        },
+        {
+            value: "Rangpur",
+        },
+        {
+            value: "Noakhali",
+        },
+        {
+            value: "Khulna",
+        },
+        {
+            value: "Tangail",
+        },
+        {
+            value: "Panchagarh",
+        },
+        {
+            value: "Bhola",
+        },
+        {
+            value: "Bandarban",
+        },
+        {
+            value: "Chandpur",
+        },
+        {
+            value: "Habiganj",
+        },
+        {
+            value: "Lakshmipur",
+        },
+        {
+            value: "Barguna",
+        },
+        {
+            value: "Jhalokati",
+        },
+        {
+            value: "Pirojpur",
+        },
+        {
+            value: "Patuakhali",
+        },
+        {
+            value: "Jhenaidah",
+        },
+        {
+            value: "Narail",
+        },
+        {
+            value: "Magura",
+        },
+        {
+            value: "Lalmonirhat",
+        },
+        {
+            value: "Kurigram",
+        },
+        {
+            value: "Nilphamari",
+        },
+        {
+            value: "Gaibandha",
+        },
+        {
+            value: "Thakurgaon",
+        },
+        {
+            value: "Satkhira",
+        },
+        {
+            value: "Bagerhat",
+        },
+        {
+            value: "Chuadanga",
+        },
+        {
+            value: "Meherpur",
+        },
+        {
+            value: "Sirajganj",
+        },
+        {
+            value: "Joypurhat",
+        },
+        {
+            value: "Natore",
+        },
+        {
+            value: "Naogaon",
+        },
+        {
+            value: "Nawabganj",
+        },
+        {
+            value: "Khagrachhari",
+        },
+        {
+            value: "Feni",
+        },
+        {
+            value: "Brahmanbaria",
+        },
+        {
+            value: "Sunamganj",
+        },
+        {
+            value: "Cox's Bazar",
+        },
+        {
+            value: "Moulvibazar",
+        },
+        {
+            value: "Gopalganj",
+        },
+        {
+            value: "Shariatpur",
+        },
+        {
+            value: "Madaripur",
+        },
+        {
+            value: "Rajbari",
+        },
+        {
+            value: "Gazipur",
+        },
+        {
+            value: "Kishoreganj",
+        },
+        {
+            value: "Jamalpur",
+        },
+        {
+            value: "Sherpur",
+        },
+        {
+            value: "Netrakona",
+        },
+        {
+            value: "Munshiganj",
+        },
+        {
+            value: "Narsingdi",
+        },
+        {
+            value: "Narayanganj",
+        },
+        {
+            value: "Manikganj",
+        },
+    ];
+
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
@@ -404,16 +599,9 @@ export const AccordionPartner = ({ data, isLoading }) => {
                                 onChange={handleHometownChange}
                                 placeholder="Select Hometown"
                                 showSearch
-                                filterOption={(input, option) => (option?.children ?? "").toLowerCase().includes(input.toLowerCase())}
-                            >
-                                {homeTowns.map(town => {
-                                    return (
-                                        <Select.Option key={town.id} value={town.value}>
-                                            {town.name}
-                                        </Select.Option>
-                                    );
-                                })}
-                            </Select>
+                                filterOption={(inputValue, option) => option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1}
+                                options={hometown}
+                            ></Select>
                         </div>
                         <div>
                             <h1 className="text-base leading-6 font-medium mb-2">Current Location</h1>
