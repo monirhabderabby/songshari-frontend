b8fc1ab6a64db249091a9e67a1d8e6827c7d9842
Md-Tur <turibnmostafiz@gmail.com>
2023-01-04 21:18:25 +0600

    matches loader added accordion problem fix

--- src/components/SingleProfilesUser/profileSection/Matches.js
+++ src/components/SingleProfilesUser/profileSection/Matches.js
@@ -3,33 +3,69 @@ import React, { useState } from "react";
 import { BiUserCircle } from "react-icons/bi";
 import { TbMessage } from "react-icons/tb";
 
+// 3rd party package
+import { useGetMatchedUsersQuery } from "../../../Redux/features/connections/connectionApi";
+import { AiOutlineWarning } from "react-icons/ai";
+// import { FiUsers } from "react-icons/fi";
+
 // components import
 import { ageCalculator } from "../../../assets/utilities/AgeCalculation/ageCalculator";
 import MatchersModalForm from "../MatchPreference/MatchersModalForm";
-// 3rd party package
-import { useGetMatchedUsersQuery } from "../../../Redux/features/connections/connectionApi";
+import { MatchesLoader } from "../../shared/Cards/Loader/Matches/MatchesLoader";
 
 // css files
 import "./Matches.css";
 
 export const Matches = () => {
     // hooks variables
-    const { data, isLoading } = useGetMatchedUsersQuery();
+    const { data, isLoading, error } = useGetMatchedUsersQuery();
     const [mathchesModalOpen, setMatchesModalOpen] = useState(false);
 
-    if (isLoading) {
-        return;
-    }
+    let content = null;
 
-    return (
-        <div className="w-full lg:max-w-[521px] mx-auto">
-            <button
-                className="bg-[linear-gradient(166deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)] text-white p-2 rounded"
-                onClick={() => setMatchesModalOpen(true)}
-            >
-                Matches Modal
-            </button>
-            <div className="grid lg:grid-cols-2 gap-4">
+    if (isLoading) {
+        content = (
+            <div className="flex justify-center gap-4 w-full">
+                <MatchesLoader />
+                <MatchesLoader />
+            </div>
+        )
+    }
+    /* else if (!isLoading && isError && error?.data?.message?.includes("No match preference")) {
+        content = (
+            <div className="flex items-center justify-center mt-[30%] w-full">
+                <FiUsers className="text-[48px] text-gray-400" />
+                <p className="mt-[10px] text-[22px] font-Inter font-medium text-gray-500">No Matches Found!</p>
+            </div>
+        );
+    }  */
+    /* else if (!isLoading && isError && error?.data?.message?.includes("Cannot check authentication")) {
+        content = (
+            <div className="flex items-center justify-center mt-[30%] w-full">
+                <AiOutlineWarning className="text-[48px] text-gray-400" />
+                <p className="mt-[10px] text-[22px] font-Inter font-medium text-gray-500">Authentication failed! Try again.</p>
+            </div>
+        );
+    }  */
+    else if (!isLoading && error?.status?.includes("PARSING_ERROR")) {
+        content = (
+            <div className="flex items-center justify-center mt-[30%] w-full">
+                <AiOutlineWarning className="text-[48px] text-gray-400" />
+                <p className="mt-[10px] text-[22px] font-Inter font-medium text-gray-500">Server Error</p>
+            </div>
+        );
+    }
+    /* else if (!isLoading && data?.data?.user?.length === 0) {
+        content = (
+            <div className="flex items-center justify-center mt-[30%] w-full">
+                <FiUsers className="text-[48px] text-gray-400" />
+                <p className="mt-[10px] text-[22px] font-Inter font-medium text-gray-500">No User Found!</p>
+            </div>
+        );
+    }  */
+    else if (!isLoading && data?.data?.user?.length > 0) {
+        content = (
+            <div className="w-full grid grid-cols-1 gap-y-4 px-4">
                 {data?.data?.users.map((info, index) => {
                     return (
                         <div key={index} className=" rounded-lg lg:max-w-lg shadow-xl colorrrrrr">
@@ -53,6 +89,20 @@ export const Matches = () => {
                         </div>
                     );
                 })}
+            </div>
+        );
+    }
+
+    return (
+        <div className="w-full lg:max-w-[521px] mx-auto">
+            <button
+                className="bg-[linear-gradient(166deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)] text-white p-2 rounded mb-3"
+                onClick={() => setMatchesModalOpen(true)}
+            >
+                Matches Modal
+            </button>
+            <div className="w-full grid lg:grid-cols-2 gap-4">
+                {content}
             </div>
             <MatchersModalForm {...{ mathchesModalOpen, setMatchesModalOpen }} />
         </div>
