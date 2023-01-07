d4cafa8b2dc100582078a3500a024dbabc86b8a8
Monir <monirhrabby.personal@gmail.com>
2023-01-07 15:26:45 +0600

    page change

--- src/Wrapper/Stepper/Container.js
+++ src/Wrapper/Stepper/Container.js
@@ -10,7 +10,7 @@ import { PhysicalDetails } from "./PhysicalDetails";
 import { ProfessionalDetails } from "./ProfessionalDetails";
 
 export const Container = () => {
-    const [page, setPage] = useState(3);
+    const [page, setPage] = useState(1);
     return (
         <div className="min-h-screen">
             <NavBar />
