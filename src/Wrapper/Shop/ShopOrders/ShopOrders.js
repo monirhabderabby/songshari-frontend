fb94eba032d6b62db6a9c81406a7869eb6d09e28
mobashirul-alam <mobashirulalamseam@gmail.com>
2023-01-18 17:45:47 +0600

    shop nav created

---
+++ src/Wrapper/Shop/ShopOrders/ShopOrders.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ShopOrders = () => {
+  return (
+    <div>
+      <h1 className="text-center text-5xl font-thin">
+        Your Orders Page is under construction
+      </h1>
+      <h2 className="text-center text-5xl font-thin mt-3">
+        Stay with us to find your orders solution
+      </h2>
+    </div>
+  );
+};
+
+export default ShopOrders;
