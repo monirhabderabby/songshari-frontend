81888480f4448ef615157e063e0a273feea8fc2b
Md Robin Hossain <hossainmdrobin967@gmail.com>
2023-01-21 12:38:11 -0600

    Error fixed for cannot read pro..

--- src/components/pages/Membership/MobileVersion/MobileDiamondPlan.js
+++ src/components/pages/Membership/MobileVersion/MobileDiamondPlan.js
@@ -14,7 +14,9 @@ const MobileDiamondPlan = () => {
 
   useEffect(() => {
     data?.data?.plans?.forEach((element) => {
-      if (element?.title?.toLowerCase() === paramsObj.get("plan").toLowerCase()) {
+      if (
+        element?.title?.toLowerCase() === paramsObj.get("plan").toLowerCase()
+      ) {
         setPlan(element);
       }
     });
@@ -71,23 +73,28 @@ const MobileDiamondPlan = () => {
           ))}
         </div>
         <div className="flex justify-center pb-5 mb-10 relative">
-          <form action="">
-          <button type="submit" action={`http://localhost:4000/payment/plan?_plan=${
-                plan._id
-              }&amount=${plan.priceMonth.toString()}&desc=${"Plan payment"}&_token=${getCookie(
-                "token"
-              )}`}
-              method="post"
-            className="py-6 px-24 rounded-[96px] text-lg leading-5 font-bold text-white"
-            style={{zIndex:100,
-              backgroundImage:
-                "linear-gradient(180deg, #CC1A7A 0%, #5A3FA0 96.35%, rgba(228, 18, 114, 0) 100%)",
-            }}
+          <form
+            type="submit"
+            action={`http://localhost:4000/payment/plan?_plan=${
+              plan?._id
+            }&amount=${plan?.priceMonth.toString()}&desc=${"Plan payment"}&_token=${getCookie(
+              "token"
+            )}`}
+            method="post"
           >
-            Get Started
-          </button>
+            <button
+              className="py-6 px-24 rounded-[96px] text-lg leading-5 font-bold text-white"
+              style={{
+                zIndex: 100,
+                backgroundImage:
+                  "linear-gradient(180deg, #CC1A7A 0%, #5A3FA0 96.35%, rgba(228, 18, 114, 0) 100%)",
+              }}
+            >
+              Get Started
+            </button>
           </form>
-          <img style={{zIndex:-1}}
+          <img
+            style={{ zIndex: -1 }}
             className="absolute bottom-0 rotate-180 h-36 w-full"
             src={topShape}
             alt="Not Available"
