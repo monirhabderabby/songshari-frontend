1671bb95a43e1a6e3a123028d8726070ad3dd31a
Monir <monirhrabby.personal@gmail.com>
2023-01-24 16:32:33 +0600

    a

--- src/components/pages/Checkout/CheckoutPage.js
+++ src/components/pages/Checkout/CheckoutPage.js
@@ -8,34 +8,34 @@ import { LeftSideCheckoutContainer } from "./LeftSideCheckoutContainer/LeftSideC
 import { RightSideCheckoutContainer } from "./RightSideCheckoutContainer/RightSideCheckoutContainer";
 
 export const CheckoutPage = () => {
-  const dispatch = useDispatch();
-  const authInfo = useSelector(
-    (state) => state.persistedReducer?.userInfo?.userInfo?.user
-  );
-  const { firstName, lastName } = authInfo || {};
-  const token = getCookie("token");
-  const { email } = decodeToken(token) || {};
+    const dispatch = useDispatch();
+    const authInfo = useSelector(state => state.persistedReducer?.userInfo?.userInfo?.user);
+    const { firstName, lastName } = authInfo || {};
+    const token = getCookie("token");
+    const { email } = decodeToken(token) || {};
 
-  useEffect(() => {
-    dispatch(
-      setBillingDetailesInitialState({
-        firstName,
-        lastName,
-        email,
-      })
-    );
-  }, [dispatch, firstName, lastName, email]);
-  return (
-    <div className="bg-[#FAFBFF] min-h-[2700px]">
-      <CustomHeader title="Checkout" />
-      <section className="max-w-[1200px] mx-auto px-[20px] mt-[81px]">
-        <div className="w-full grid grid-cols-2 gap-x-[24px]">
-          <div className="left_side w-[570px] h-[430px] mx-auto bg-[#FAFBFF]">
-            <LeftSideCheckoutContainer />
-          </div>
-          <div className="right_Side w-[370px] h-[430px] mx-auto">
-            <RightSideCheckoutContainer />
-          </div>
+    useEffect(() => {
+        dispatch(
+            setBillingDetailesInitialState({
+                firstName,
+                lastName,
+                email,
+            })
+        );
+    }, [dispatch, firstName, lastName, email]);
+    return (
+        <div className="bg-[#FAFBFF] min-h-[2700px]">
+            <CustomHeader title="Checkout" />
+            <section className="max-w-[1200px] mx-auto px-[20px] mt-[81px]">
+                <div className="w-full grid grid-cols-2 gap-x-[24px]">
+                    <div className="left_side w-[570px] h-[430px] mx-auto bg-[#FAFBFF]">
+                        <LeftSideCheckoutContainer />
+                    </div>
+                    <div className="right_Side w-[370px] h-[430px] mx-auto">
+                        <RightSideCheckoutContainer />
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
