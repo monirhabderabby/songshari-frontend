ee4fe3c6d4c4102c0f2b2c7e172a79846a6aef79
Monir <monirhrabby.personal@gmail.com>
2023-01-26 10:31:02 +0600

    added info and connection routing

--- src/components/pages/Kazi/Profile/profileContainer/KaziTable.js
+++ src/components/pages/Kazi/Profile/profileContainer/KaziTable.js
@@ -1,6 +1,5 @@
 // configuration
 import React, { useState } from "react";
-import { useNavigate } from "react-router";
 
 // components
 import { Friends } from "../../../../SingleProfilesUser/FriendsSection/Friends";
@@ -12,13 +11,12 @@ import "../../../../../App.css";
 const KaziTable = ({ data, isLoading }) => {
     const [page, setPage] = useState(2);
     //here navigate use for manage edit proofile information route
-    const navigate = useNavigate();
 
     return (
         <div>
             <div className="mb-4 w-full">
                 <div className="w-full h-[60px] lg:max-w-[512px] mx-auto bg-white mb-[20px] shadow-[rgba(100,100,111,0.2)_0px_7px_29px_0px] rounded-[4px]">
-                    <div className="flex justify-center items-center h-full gap-4">
+                    {/* <div className="flex justify-center items-center h-full gap-4">
                         <button
                             className={`font-medium text-[15px] leading-tight ${page === 1 ? "text-primary font-bold" : "text-gray-800"}`}
                             onClick={() => setPage(1)}
@@ -28,9 +26,26 @@ const KaziTable = ({ data, isLoading }) => {
                         <button
                             className={`font-medium text-[15px] leading-tight ${page === 2 ? "text-primary font-bold" : "text-gray-800"}`}
                             onClick={() => {
-                                navigate("/userprofile");
                                 setPage(2);
                             }}
+                        >
+                            Connection
+                        </button>
+                    </div> */}
+                    <div className="w-full h-full flex justify-between">
+                        <button
+                            className={`flex-1 text-gray-800 rounded-tl-[4px] rounded-bl-[4px] font-medium text-[15px] duration-200 ${
+                                page === 1 && "bg-[linear-gradient(166deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)] text-white font-bold"
+                            }`}
+                            onClick={() => setPage(1)}
+                        >
+                            Info
+                        </button>
+                        <button
+                            className={`flex-1 text-gray-800 rounded-tr-[4px] rounded-br-[4px] font-medium duration-200 text-[15px] ${
+                                page === 2 && "bg-[linear-gradient(166deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)] text-white font-bold"
+                            }`}
+                            onClick={() => setPage(2)}
                         >
                             Connection
                         </button>
