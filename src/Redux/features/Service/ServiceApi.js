217a920afa315d6a149012e7b4dcd256fce988d3
Md Robin Hossain <hossainmdrobin967@gmail.com>
2023-01-27 11:25:43 -0600

    all needed service api created

--- src/Redux/features/Service/ServiceApi.js
+++ src/Redux/features/Service/ServiceApi.js
@@ -13,6 +13,16 @@ export const serviceApi = apiSlice.injectEndpoints({
         body: data,
       }),
     }),
+    updateSingleService: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `/member/service/${id}`,
+        method: "PUT",
+        headers: {
+          authorization: `Bearer ${getCookie("token")}`,
+        },
+        body: data,
+      }),
+    }),
     getMyServices: builder.query({
       query: () => ({
         url: "/member/service/myServices",
@@ -22,16 +32,41 @@ export const serviceApi = apiSlice.injectEndpoints({
         },
       }),
     }),
-    getServiceById: builder.query({
-        query: (id) => ({
-          url: `/member/service/${id}`,
-          method: "GET",
-          headers: {
-            authorization: `Bearer ${getCookie("token")}`,
-          },
-        }),
+    getSuggestedServices: builder.query({
+      query: (page, keyword) => ({
+        url: `/member/service/suggested?_page=${page}&_limit=10&_keyword=${keyword}`,
+        method: "GET",
+        headers: {
+          authorization: `Bearer ${getCookie("token")}`,
+        },
       }),
+    }),
+    getServiceById: builder.query({
+      query: (id) => ({
+        url: `/member/service/${id}`,
+        method: "GET",
+        headers: {
+          authorization: `Bearer ${getCookie("token")}`,
+        },
+      }),
+    }),
+    deleteSingleService: builder.query({
+      query: (id) => ({
+        url: `/member/service/${id}`,
+        method: "DELETE",
+        headers: {
+          authorization: `Bearer ${getCookie("token")}`,
+        },
+      }),
+    }),
   }),
 });
 
-export const { useAddSingleServiceMutation,useGetMyServicesQuery, useGetServiceByIdQuery } = serviceApi;
+export const {
+  useAddSingleServiceMutation,
+  useGetMyServicesQuery,
+  useGetServiceByIdQuery,
+  useUpdateSingleServiceMutation,
+  useGetSuggestedServicesQuery,
+  useDeleteSingleServiceQuery,
+} = serviceApi;
