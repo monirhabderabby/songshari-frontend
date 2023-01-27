370d91ff248801cada59205112900dfa01c64e67
Md Robin Hossain <hossainmdrobin967@gmail.com>
2023-01-27 08:51:58 -0600

    service post get getall done

--- src/Redux/features/Service/ServiceApi.js
+++ src/Redux/features/Service/ServiceApi.js
@@ -2,18 +2,36 @@ import getCookie from "../../../Helper/cookies/getCookie";
 import apiSlice from "../../api/apiSlice";
 
 export const serviceApi = apiSlice.injectEndpoints({
-    endpoints: builder => ({
-        addSingleService: builder.mutation({
-            query: data => ({
-                url: "/member/service/add",
-                method: "POST",
-                headers: {
-                    authorization: `Bearer ${getCookie("token")}`,
-                },
-                body: data,
-            }),
-        }),
+  endpoints: (builder) => ({
+    addSingleService: builder.mutation({
+      query: (data) => ({
+        url: "/member/service/add",
+        method: "POST",
+        headers: {
+          authorization: `Bearer ${getCookie("token")}`,
+        },
+        body: data,
+      }),
     }),
+    getMyServices: builder.query({
+      query: () => ({
+        url: "/member/service/myServices",
+        method: "GET",
+        headers: {
+          authorization: `Bearer ${getCookie("token")}`,
+        },
+      }),
+    }),
+    getServiceById: builder.query({
+        query: (id) => ({
+          url: `/member/service/${id}`,
+          method: "GET",
+          headers: {
+            authorization: `Bearer ${getCookie("token")}`,
+          },
+        }),
+      }),
+  }),
 });
 
-export const { useAddSingleServiceMutation } = serviceApi;
+export const { useAddSingleServiceMutation,useGetMyServicesQuery, useGetServiceByIdQuery } = serviceApi;
