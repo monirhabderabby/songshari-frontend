78351049188aa878ca78d6036ffcfc3f356f3ecc
Monir <monirhrabby.personal@gmail.com>
2023-01-29 10:55:11 +0600

    added redirect to checkout optiojn

--- src/components/SingleProfilesUser/SingleProfiles.js
+++ src/components/SingleProfilesUser/SingleProfiles.js
@@ -18,67 +18,63 @@ import UtilitisCard from "./UtilitisCard";
 import { VerificationCard } from "./VerificationCard";
 
 export const SingleProfiles = () => {
-  // hook variable declaration
-  const [SocialBoxOpen, setSocialBoxOpen] = useState(false);
-  const { data, isLoading, error } = useGetProfileDetailsWIthAuthQuery();
-  const dispatch = useDispatch();
+    // hook variable declaration
+    const [SocialBoxOpen, setSocialBoxOpen] = useState(false);
+    const { data, isLoading, error } = useGetProfileDetailsWIthAuthQuery();
+    const dispatch = useDispatch();
 
-  // JS Variables
-  // decision making about social Box
-  const { LinkedInId } = data || {};
-  const { faceBookId } = data || {};
-  const { instagramId } = data || {};
-  useEffect(() => {
-    if (LinkedInId || faceBookId || instagramId) {
-      setSocialBoxOpen(true);
-    }
-  }, [LinkedInId, faceBookId, instagramId, setSocialBoxOpen]);
+    // JS Variables
+    // decision making about social Box
+    const { LinkedInId } = data || {};
+    const { faceBookId } = data || {};
+    const { instagramId } = data || {};
+    useEffect(() => {
+        if (LinkedInId || faceBookId || instagramId) {
+            setSocialBoxOpen(true);
+        }
+    }, [LinkedInId, faceBookId, instagramId, setSocialBoxOpen]);
 
-  useEffect(() => {
-    if (data) {
-      const photos = data?.photos;
-      dispatch(loadPhotos(photos));
-    }
-  }, [data, dispatch]);
+    useEffect(() => {
+        if (data) {
+            const photos = data?.photos;
+            dispatch(loadPhotos(photos));
+        }
+    }, [data, dispatch]);
 
-  return (
-    <div className="bg-[#FAFBFF]">
-      <CustomHeader title="Profile" />
-      <div className="custom-container mx-auto bg-[#FAFBFF] pt-[30px] ">
-        <div className="block lg:flex">
-          <div className="w-full ">
-            <div className="block lg:flex">
-              <div className="max-w-[360px]">
-                <div className="md:hidden hidden lg:block">
-                  <ProfileCard {...{ data, isLoading }} />
-                  {SocialBoxOpen && (
-                    <SocialMediaBox
-                      {...{ LinkedInId, faceBookId, instagramId }}
-                    />
-                  )}
-                  <VerificationCard />
-                  {/* <Badges /> */}
-                  <UtilitisCard />
-                  <IntroCard {...{ data, isLoading, error }} />
-                  <PhotoUploadCard />
-                  <PhotoGelary {...{ isLoading, error }} />
+    return (
+        <div className="bg-[#FAFBFF]">
+            <CustomHeader title="Profile" />
+            <div className="custom-container mx-auto bg-[#FAFBFF] pt-[30px] ">
+                <div className="block lg:flex">
+                    <div className="w-full ">
+                        <div className="block lg:flex">
+                            <div className="max-w-[360px]">
+                                <div className="md:hidden hidden lg:block">
+                                    <ProfileCard {...{ data, isLoading }} />
+                                    {SocialBoxOpen && <SocialMediaBox {...{ LinkedInId, faceBookId, instagramId }} />}
+                                    <VerificationCard />
+                                    {/* <Badges /> */}
+                                    <UtilitisCard />
+                                    <IntroCard {...{ data, isLoading, error }} />
+                                    <PhotoUploadCard />
+                                    <PhotoGelary {...{ isLoading, error }} />
+                                </div>
+                            </div>
+                            <div className="w-full hidden md:hidden lg:block">
+                                <div>
+                                    <Table {...{ data, isLoading }} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="max-w-[263px]">
+                        <div className="flex justify-center">
+                            <ChartBoard {...{ data, isLoading }} />
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <div className="w-full hidden md:hidden lg:block">
-                <div>
-                  <Table {...{ data, isLoading }} />
-                </div>
-              </div>
             </div>
-          </div>
-          <div className="max-w-[263px]">
-            <div className="flex justify-center">
-              <ChartBoard {...{ data, isLoading }} />
-            </div>
-          </div>
+            <Footer />
         </div>
-      </div>
-      <Footer />
-    </div>
-  );
+    );
 };
