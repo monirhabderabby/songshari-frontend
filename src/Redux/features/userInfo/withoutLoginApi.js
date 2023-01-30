46ec4044259a615769315ac7a095fa3e535d411c
mobashirul-alam <mobashirulalamseam@gmail.com>
2023-01-30 22:10:52 +0600

    certificate re upload completed

--- src/Redux/features/userInfo/withoutLoginApi.js
+++ src/Redux/features/userInfo/withoutLoginApi.js
@@ -1,14 +1,15 @@
 import apiSlice from "../../api/apiSlice";
 
 export const withoutLoginApi = apiSlice.injectEndpoints({
-    endpoints: builder => ({
-        getRecentMembers: builder.query({
-            query: () => "withoutLogin/recentMembers",
-        }),
-        getAllPlans: builder.query({
-            query: () => "admin/plan",
-        }),
+  endpoints: (builder) => ({
+    getRecentMembers: builder.query({
+      query: () => "withoutLogin/recentMembers",
     }),
+    getAllPlans: builder.query({
+      query: () => "admin/plan",
+    }),
+  }),
 });
 
-export const { useGetRecentMembersQuery,useGetAllPlansQuery } = withoutLoginApi;
+export const { useGetRecentMembersQuery, useGetAllPlansQuery } =
+  withoutLoginApi;
--- src/components/SingleProfilesUser/CertificateSection/UpdateCertificateSection/FileReuploadSection/FileReuploadSection.js
+++ src/components/SingleProfilesUser/CertificateSection/UpdateCertificateSection/FileReuploadSection/FileReuploadSection.js
@@ -1,46 +1,75 @@
-import React from 'react';
-/* reupload section img */
-import icon from '../../../../../assets/images/user profile/reUpload.png';
-import reUpload from '../../../../../assets/images/user profile/certificate2.png';
-import reUpload1 from '../../../../../assets/images/user profile/certificate3.png';
+import React from "react";
 
-const FileReuploadSection = () => {
-    return (
-        <div className='font-sans mb-[145px]'>
-            <h1 className='font-semibold text-[32px] text-[#333333]'>Re-Upload Certificate</h1>
-            <p className='mt-4 mb-8 font-normal text-base text-[#000000]'>Admin Ask You to upload your Certificate again</p>
-            {/* reupload section */}
-            <div className=' lg:flex items-center justify-center gap-[13px] grid grid-cols-1'>
-                <label className="cursor-pointer z-20 w-full">
-                    <div className='relative overflow-hidden h-[380px] w-full  rounded-2xl mx-auto lg:mx-0'>
-                        <img className='absolute z-[-1] w-full h-full' src={reUpload} alt="background" />
-                        <div className='z-30 w-full h-full flex flex-col items-center justify-center bg-[#000000] bg-opacity-50'>
-                            <p className='font-medium text-base text-[#FFFFFF] text-center mb-5'>Drag and drop or clilck on the upload <br /> button to add new certificate</p>
-                            <div className='flex items-center justify-center font-medium text-base text-[#FFFFFF] gap-[13px] bg-gradient-to-r from-[#E52982] to-[#A72BC3] rounded-[10px] w-[144px] h-[47px]'>
-                                <img className='h-5 w-5' src={icon} alt="reUpload icon" />
-                                <p>Re-Upload</p>
-                            </div>
-                        </div>
-                    </div>
-                    <input id="reUploadPhoto" type="file" className="hidden" />
-                </label>
+// Third party package
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { v4 as uuidv4 } from "uuid";
 
-                <label className="cursor-pointer w-full z-20">
-                    <div className='relative overflow-hidden h-[380px] w-full rounded-2xl mx-auto lg:mx-0'>
-                        <img className='absolute z-[-1] w-full h-full' src={reUpload1} alt="background" />
-                        <div className='z-30 w-full h-full flex flex-col items-center justify-center bg-[#000000] bg-opacity-50'>
-                            <p className='font-medium text-base text-[#FFFFFF] text-center mb-5'>Drag and drop or clilck on the upload <br /> button to add new certificate</p>
-                            <div className='flex items-center justify-center font-medium text-base text-[#FFFFFF] gap-[13px] bg-gradient-to-r from-[#E52982] to-[#A72BC3] rounded-[10px] w-[144px] h-[47px]'>
-                                <img className='h-5 w-5' src={icon} alt="reUpload icon" />
-                                <p>Re-Upload</p>
-                            </div>
-                        </div>
-                    </div>
-                    <input id="reUploadPhoto" type="file" className="hidden" />
-                </label>
+/* Components */
+import icon from "../../../../../assets/images/user profile/reUpload.png";
+import { firebaseStorage } from "../../../../../firebase.init";
+import { useResubmitProfessionalCertificateMutation } from "../../../../../Redux/features/Documents/documentsApi";
+import { toast, Toaster } from "react-hot-toast";
+
+const FileReuploadSection = ({ selectedCertificate }) => {
+  const [resubmitProfessionalCertificate, { data: response, isLoading }] =
+    useResubmitProfessionalCertificateMutation();
+
+  // handle file upload change data
+  const handleUpload = async (e) => {
+    const photo = e.target.files[0];
+    const storageRef = ref(firebaseStorage, `cover/${photo?.name + uuidv4()}`);
+    uploadBytes(storageRef, photo).then(async (snapshot) => {
+      await getDownloadURL(snapshot.ref).then((url) => {
+        resubmitProfessionalCertificate({
+          id: selectedCertificate?._id,
+          specialAchievementMoment: url.toString(),
+        });
+      });
+    });
+  };
+  if (response) {
+    toast.success("Successfully Re-uploaded");
+  }
+
+  return (
+    <div className="font-sans mb-[145px]">
+      <h1 className="font-semibold text-[32px] text-[#333333]">
+        Re-Upload Certificate
+      </h1>
+      <p className="mt-4 mb-8 font-normal text-base text-[#000000]">
+        Admin Ask You to upload your Certificate again
+      </p>
+      {/* reupload section */}
+      <div className=" lg:flex items-center justify-center max-w-[393px]">
+        <label htmlFor="reUploadPhoto" className="cursor-pointer z-20 w-full">
+          <div className="relative overflow-hidden h-[380px] w-full  rounded-2xl mx-auto lg:mx-0">
+            <img
+              className="absolute z-[-1] w-full h-full"
+              src={selectedCertificate?.specialAchievementMoment}
+              alt="background"
+            />
+            <div className="z-30 w-full h-full flex flex-col items-center justify-center bg-[#000000] bg-opacity-50">
+              <p className="font-medium text-base text-[#FFFFFF] text-center mb-5">
+                Drag and drop or click on the upload <br /> button to add new
+                certificate
+              </p>
+              <div className="flex items-center justify-center font-medium text-base text-[#FFFFFF] gap-[13px] bg-gradient-to-r from-[#E52982] to-[#A72BC3] rounded-[10px] w-[144px] h-[47px]">
+                <img className="h-5 w-5" src={icon} alt="reUpload icon" />
+                <p>{isLoading ? "Uploading..." : "Re-Upload"}</p>
+              </div>
             </div>
-        </div>
-    );
+          </div>
+          <input
+            id="reUploadPhoto"
+            onChange={handleUpload}
+            type="file"
+            className="hidden"
+          />
+        </label>
+      </div>
+      <Toaster />
+    </div>
+  );
 };
 
 export default FileReuploadSection;
