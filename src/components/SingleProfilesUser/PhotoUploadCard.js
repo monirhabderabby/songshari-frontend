78da80c7a1cb3e688c86fc13d735d004b66aff13
mobashirul-alam <mobashirulalamseam@gmail.com>
2023-01-30 18:33:03 +0600

    profile problem solved and findapartnersuggested warning solved

--- src/components/SingleProfilesUser/PhotoUploadCard.js
+++ src/components/SingleProfilesUser/PhotoUploadCard.js
@@ -13,70 +13,84 @@ import { useUpdatePhotosOnProfileMutation } from "../../Redux/features/userInfo/
 import { setUpdatePhotos } from "../../Redux/features/userInfo/userInfo";
 
 export const PhotoUploadCard = () => {
-    // Redux Api call
-    const [photosUploadOnServer, { data: uploadedPhotos, isLoading, error: uploadError }] = usePhotosUploadOnServerMutation();
-    const [updatePhotosOnProfile, { error: updateError }] = useUpdatePhotosOnProfileMutation();
-    const dispatch = useDispatch();
+  // Redux Api call
+  const [
+    photosUploadOnServer,
+    { data: uploadedPhotos, isLoading, error: uploadError },
+  ] = usePhotosUploadOnServerMutation();
+  const [updatePhotosOnProfile, { error: updateError }] =
+    useUpdatePhotosOnProfileMutation();
+  const dispatch = useDispatch();
 
-    // function declaration
-    const photosHandler = e => {
-        const files = e.target.files;
-        const formData = new FormData();
-        for (let i = 0; i < files.length; i++) {
-            formData.append("photos", files[i]);
-        }
-        // console.log(formData.get("photos"));
-        photosUploadOnServer(formData);
-    };
+  // function declaration
+  const photosHandler = (e) => {
+    const files = e.target.files;
+    const formData = new FormData();
+    for (let i = 0; i < files.length; i++) {
+      formData.append("photos", files[i]);
+    }
+    // console.log(formData.get("photos"));
+    photosUploadOnServer(formData);
+  };
 
-    useEffect(() => {
-        if (uploadedPhotos) {
-            let updateElement = [];
-            uploadedPhotos?.data?.file?.map(element => {
-                return (updateElement = [...updateElement, element.fileName]);
-            });
+  useEffect(() => {
+    if (uploadedPhotos) {
+      let updateElement = [];
+      uploadedPhotos?.data?.file?.map((element) => {
+        return (updateElement = [...updateElement, element.fileName]);
+      });
 
-            // update photo in redux state
-            dispatch(setUpdatePhotos(updateElement));
+      // update photo in redux state
+      dispatch(setUpdatePhotos(updateElement));
 
-            // update all uploaded photos in user personal Details
-            updatePhotosOnProfile({
-                photos: updateElement,
-            });
-        }
-    }, [uploadedPhotos, updatePhotosOnProfile, dispatch]);
+      // update all uploaded photos in user personal Details
+      updatePhotosOnProfile({
+        photos: updateElement,
+      });
+    }
+  }, [uploadedPhotos, updatePhotosOnProfile, dispatch]);
 
-    useEffect(() => {
-        if (uploadError || updateError) {
-            toast.error("Failed to upload photos! ");
-        }
-    }, [uploadError, updateError]);
+  useEffect(() => {
+    if (uploadError || updateError) {
+      toast.error("Failed to upload photos! ");
+    }
+  }, [uploadError, updateError]);
 
-    return (
-        <>
-            <label
-                htmlFor="photos"
-                className="mt-[24px] w-[360px] h-[77px] bg-white shadow-[0px_10px_5px_rgba(119,123,146,0.02)] rounded-[10px] py-[16px] px-[20px] flex items-center gap-x-[9px] cursor-pointer"
-            >
-                {isLoading ? (
-                    <TailSpin
-                        height="34"
-                        width="34"
-                        color="#F664BC"
-                        ariaLabel="tail-spin-loading"
-                        radius="1"
-                        wrapperStyle={{}}
-                        wrapperclassName=""
-                        visible={true}
-                    />
-                ) : (
-                    <img src={camera} alt="camera" />
-                )}
-                <p className="text-[#000000] text-[24px] leading-[20px] font-semibold font-fira">
-                    {isLoading ? <p className="ml-[10px]">Uploading</p> : <p className="ml-[10px]">Upload Photo</p>}
-                </p>
-            </label>
-            <input type="file" id="photos" className="hidden" multiple onChange={photosHandler} />
-        </>
-    );
+  return (
+    <>
+      <label
+        htmlFor="photos"
+        className="mt-[24px] w-[360px] h-[77px] bg-white shadow-[0px_10px_5px_rgba(119,123,146,0.02)] rounded-[10px] py-[16px] px-[20px] flex items-center gap-x-[9px] cursor-pointer"
+      >
+        {isLoading ? (
+          <TailSpin
+            height="34"
+            width="34"
+            color="#F664BC"
+            ariaLabel="tail-spin-loading"
+            radius="1"
+            wrapperStyle={{}}
+            wrapperclassName=""
+            visible={true}
+          />
+        ) : (
+          <img src={camera} alt="camera" />
+        )}
+        <div className="text-[#000000] text-[24px] leading-[20px] font-semibold font-fira">
+          {isLoading ? (
+            <p className="ml-[10px]">Uploading</p>
+          ) : (
+            <p className="ml-[10px]">Upload Photo</p>
+          )}
+        </div>
+      </label>
+      <input
+        type="file"
+        id="photos"
+        className="hidden"
+        multiple
+        onChange={photosHandler}
+      />
+    </>
+  );
 };
