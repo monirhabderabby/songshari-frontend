e1fa3e0b08f67ec860ee0b312253b7e5ff14aa84
mobashirul-alam <mobashirulalamseam@gmail.com>
2023-01-31 17:52:20 +0600

    membership card details added and design edited

--- src/components/pages/Membership/membershippremium/MembershipPremium.js
+++ src/components/pages/Membership/membershippremium/MembershipPremium.js
@@ -1,14 +1,20 @@
+// Configuration
 import React from "react";
-import "../../../../assets/css/MembershipPremium.css";
+
+// Components
 import { useGetAllPlansQuery } from "../../../../Redux/features/userInfo/withoutLoginApi";
 import MemberShipPremiumCard from "../../../shared/Cards/MemberShipPremiumCards/MemberShipPremiumCard";
+
+// CSS file
+import "../../../../assets/css/MembershipPremium.css";
+
 export const MembershipPremium = () => {
   const { data, isLoading, error } = useGetAllPlansQuery();
 
   return (
     <div className="premium-membership ">
       <div className="max-w-[1200px] mx-auto">
-        <div className="grid lg:grid-cols-4 sm:grid-cols-1 gap-6 mx-auto ml-28 mr-28 mt-20">
+        <div className="grid lg:grid-cols-4 sm:grid-cols-1 gap-6 mx-24 mt-20">
           {!isLoading &&
             !error &&
             data?.data?.plans?.map((item) => (
@@ -17,10 +23,10 @@ export const MembershipPremium = () => {
         </div>
       </div>
       <div className="relative">
-        <h1 className="text-white text-3xl text-center pb-3">
+        <h1 className="text-white text-3xl text-center py-10">
           If you have any questions Contact Us
         </h1>
-        <div className="absolute -top-[420px] w-full">
+        <div className="absolute -top-[590px] w-full">
           <div className="ml-10 mr-2 flex justify-between">
             <img
               alt="Not Available"
