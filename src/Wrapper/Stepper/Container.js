1b43545807dc7553e44c3c5de27dbda083844356
junaid9011 <junaid.cou@gmail.com>
2023-01-31 11:24:32 +0600

    done

--- src/Wrapper/Stepper/Container.js
+++ src/Wrapper/Stepper/Container.js
@@ -12,6 +12,7 @@ import { ProfessionalDetails } from "./ProfessionalDetails";
 import SiblingDetails from "./SiblingDetails";
 
 export const Container = () => {
+<<<<<<< HEAD
   const [page, setPage] = useState(1);
   return (
     <div className="min-h-screen">
@@ -29,6 +30,25 @@ export const Container = () => {
               {page === 7 && <OtherDetails {...{ setPage }} />}
             </div>
           </div>
+=======
+    const [page, setPage] = useState(1);
+    return (
+        <div className="min-h-screen">
+            <NavBar />
+            <section className="flex justify-center items-center w-full flex-1 text-center px-3 md:px-20 min-h-[calc(100vh-90px)]">
+                <div className="bg-white shadow-2xl rounded-2xl md:flex w-[100%] md:w-3/4 lg:w-4/5 max-w-6xl relative">
+                    <div className="w-full p-5 my-auto">
+                        <div className="my-[20px]">
+                            {page === 1 && <PersonalDetails {...{ setPage }} />}
+                            {page === 2 && <EducationalDetails {...{ setPage }} />}
+                            {page === 3 && <ProfessionalDetails {...{ setPage }} />}
+                            {page === 4 && <PhysicalDetails {...{ setPage }} />}
+                            {page === 5 && <OtherDetails {...{ setPage }} />}
+                        </div>
+                    </div>
+                </div>
+            </section>
+>>>>>>> d4cafa8b (page change)
         </div>
       </section>
     </div>
