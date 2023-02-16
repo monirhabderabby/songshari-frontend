5dedc0adf84945a84ee2c3012c4cfb2ae5fc18b6
Monir <monirhrabby.personal@gmail.com>
2023-02-16 06:56:14 +0600

    Header updated for activity order

--- src/components/pages/Agent/AgentActivity/AgentActivity.js
+++ src/components/pages/Agent/AgentActivity/AgentActivity.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
+import ActivityCompletedOrder from "../../../shared/Components/LawyerAgentKaziService/Activity/ActivityCompletedOrder";
 import ActivityHeader from "../../../shared/Components/LawyerAgentKaziService/Activity/ActivityHeader";
 import ActivityPendingOrder from "../../../shared/Components/LawyerAgentKaziService/Activity/ActivityPendingOrder";
 import ActivityRunningOrder from "../../../shared/Components/LawyerAgentKaziService/Activity/ActivityRunningOrder";
 
 // Components
-import AgentActivityCompletedCase from "./AgentActivityCompletedCase/AgentActivityCompletedCase";
 
 const AgentActivity = () => {
+    // Hook variables
     const [runningOrder, setRunningOrder] = useState(true);
     const [pendingOrder, setPendingOrder] = useState(true);
+    const [completedOrder, setCompletedOrder] = useState(true);
+
     return (
         <div className="lg:max-w-[1000px] xl:max-w-[1200px] mx-auto">
             {/* Activity basic details */}
@@ -21,7 +24,7 @@ const AgentActivity = () => {
             {runningOrder && <ActivityRunningOrder {...{ setRunningOrder }} />}
 
             {/* Completed case */}
-            <AgentActivityCompletedCase />
+            {completedOrder && <ActivityCompletedOrder {...{ setCompletedOrder }} />}
         </div>
     );
 };
--- src/components/shared/Cards/LawyerAgentKaziService/CompletedOrderCard.js
+++ src/components/shared/Cards/LawyerAgentKaziService/CompletedOrderCard.js
@@ -2,16 +2,18 @@ import moment from "moment";
 import React from "react";
 import { AiOutlineClockCircle } from "react-icons/ai";
 
-export const CompletedOrderCard = ({ order }) => {
+export const CompletedOrderCard = ({ order, activity }) => {
     const { service, updatedAt } = order || {};
     let { title, price } = service || {};
     title = title?.length > 42 ? title.slice(0, 32) + "..." : title;
     console.log(order);
     return (
         <div
-            className="rounded-xl p-6 w-[393px] h-[188px]"
+            className={`rounded-xl p-6 w-[393px] h-[188px] ${activity ? "text-white" : "text-black"}`}
             style={{
-                backgroundImage: "linear-gradient(180deg, #F4FFF5 0%, #FFFBF1 100%)",
+                backgroundImage: `${
+                    activity ? "linear-gradient(180deg, #690E94 0%, #192C6F 100%)" : "linear-gradient(180deg, #F4FFF5 0%, #FFFBF1 100%)"
+                }`,
                 boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
             }}
         >
@@ -24,7 +26,7 @@ export const CompletedOrderCard = ({ order }) => {
                     />
                 </div>
                 <div className="col-span-2 ml-[25px] my-auto">
-                    <p className="font-medium leading-6 text-[16px] text-black mb-[16px]">{title}</p>
+                    <p className="font-normal leading-6 text-[16px] mb-[16px]">{title}</p>
                     <p className="font-medium text-[18px] leading-6">$ {price}</p>
                 </div>
             </div>
@@ -32,7 +34,7 @@ export const CompletedOrderCard = ({ order }) => {
                 <span>
                     <AiOutlineClockCircle />
                 </span>
-                <p>{moment(updatedAt).format("LT")}</p>
+                <p className="font-light">{moment(updatedAt).format("LT")}</p>
             </div>
         </div>
     );
