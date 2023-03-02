f66211dbcae32f3ff65a2ce2bf8e7c55e5e68c13
mobashirul-alam <mobashirulalamseam@gmail.com>
2023-03-02 21:04:57 +0600

    mobile post time edited & conflict error solved

--- src/components/pages/DynamicProfilePage/SinglePostCard.js
+++ src/components/pages/DynamicProfilePage/SinglePostCard.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 
 // Third party package
-import moment from "moment";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import { AiFillHeart } from "react-icons/ai";
@@ -49,83 +48,32 @@ const SinglePostCard = ({ post }) => {
     }
   }, [likeResponse, errorLike]);
 
-    return (
-        <div>
-            <div className="max-w-[457px] mx-auto shadow-[2px_2px_4px_rgba(0,0,0,0.12)] bg-white rounded-[10px] p-[30px] hidden md:block">
-                <div className="flex w-full justify-between items-center">
-                    <div className="flex items-center">
-                        {post?.author?.profilePhoto ? (
-                            <img className="w-[40px] h-[40px] rounded-full mr-[19px]" src={post?.author?.profilePhoto} alt="profile" />
-                        ) : (
-                            <img
-                                className="w-[40px] h-[40px] rounded-full mr-[19px]"
-                                src="https://cdn-icons-png.flaticon.com/512/194/194938.png"
-                                alt="profile"
-                            />
-                        )}
-                        <p className="font-semibold font-fira text-[18px] leading-7 text-[#333333] mr-[10px]">
-                            {post?.author?.firstName} {post?.author?.lastName}
-                        </p>
-                        <div className="w-[20px] h-[20px] bg-[#FCE9F3] rounded-full mr-[14px]"></div>
-                        <div>
-                            <span className="text-[14px] leading-7 font-normal text-[#333333]">
-                                <span>{timeAgo(post?.createdAt)}</span>
-                            </span>
-                        </div>
-                    </div>
-                    <div>
-                        <BsThreeDotsVertical
-                            id="long-button"
-                            aria-controls={open ? "long-button" : undefined}
-                            aria-haspopup="true"
-                            aria-expanded={open ? "true" : undefined}
-                            onClick={handleClick}
-                            className="hover:text-black cursor-pointer"
-                        />
-                        <Menu
-                            id="basic-menu"
-                            anchorEl={anchorEl}
-                            open={open}
-                            onClose={handleClose}
-                            MenuListProps={{
-                                "aria-labelledby": "basic-button",
-                            }}
-                        >
-                            <MenuItem onClick={handleClose}>Edit</MenuItem>
-                            <MenuItem onClick={handleDelete}>Delete</MenuItem>
-                        </Menu>
-                    </div>
-                </div>
-                <div className="">
-                    <p className="text-[#333333] my-6 ml-[60px] mr-[6px] text-justify">{post?.postBody}</p>
-                    {post?.attachment?.length > 0 && (
-                        <img className="mt-[23px] max-h-[390px] w-full" src={liveLinkGenerator(post?.attachment[0])} alt="" />
-                    )}
-                </div>
-                <hr />
-                <div className="h-[61px] flex items-end">
-                    <div className="flex items-center">
-                        {postLiked || post?.likes?.length !== 0 ? (
-                            <div
-                                onClick={() => {
-                                    setPostLiked(false);
-                                    handleLikePost();
-                                }}
-                            >
-                                <AiFillHeart className="text-red-400 w-[24px] h-[24px] mr-[22px] text-xl transition-all duration-500 " />
-                            </div>
-                        ) : likeLoading ? (
-                            <div className="h-[16px] bg-[#E41272] rounded-full animate-ping w-[16px] mr-[22px] text-[#E41272] transition-all duration-500 "></div>
-                        ) : (
-                            <button onClick={handleLikePost}>
-                                <img src={love} alt="likeButton" className="mr-[22px]" />
-                            </button>
-                        )}
-                        <button className="border-[1px] border-[rgba(0,0,0,0.1)] rounded-[50px] px-[16px] text-[13px] leading-[30px] text-[#333333]">
-                            Comment
-                        </button>
-                    </div>
-                </div>
+  return (
+    <div>
+      <div className="max-w-[457px] mx-auto shadow-[2px_2px_4px_rgba(0,0,0,0.12)] bg-white rounded-[10px] p-[30px] hidden md:block">
+        <div className="flex w-full justify-between items-center">
+          <div className="flex items-center">
+            {post?.author?.profilePhoto ? (
+              <img
+                className="w-[40px] h-[40px] rounded-full mr-[19px]"
+                src={post?.author?.profilePhoto}
+                alt="profile"
+              />
+            ) : (
+              <img
+                className="w-[40px] h-[40px] rounded-full mr-[19px]"
+                src="https://cdn-icons-png.flaticon.com/512/194/194938.png"
+                alt="profile"
+              />
+            )}
+            <p className="font-semibold font-fira text-[18px] leading-7 text-[#333333] mr-[10px]">
+              {post?.author?.firstName} {post?.author?.lastName}
+            </p>
+            <div className="w-[20px] h-[20px] bg-[#FCE9F3] rounded-full mr-[14px]"></div>
+            <div>
+              <span className="text-[14px] leading-7 font-normal text-[#333333]">
+                <span>{timeAgo(post?.createdAt)}</span>
+              </span>
             </div>
           </div>
           <div>
