cee96168a610b44dfe71e55433b58f1458a531b6
Monir <monirhrabby.personal@gmail.com>
2023-03-07 21:02:51 +0600

    a

--- src/components/shared/Cards/Shop/ProductListingCard.js
+++ src/components/shared/Cards/Shop/ProductListingCard.js
@@ -1,11 +1,13 @@
 // configuration
 import React from "react";
+import { useNavigate } from "react-router";
 
 // components
 import img from "../../../../assets/images/shop/Listing/Rectangle.png";
 
 const ProductListingCard = ({ product }) => {
-    const { name, description, price } = product || {};
+    const navigate = useNavigate();
+    const { name, description, price, _id } = product || {};
 
     // descript length cut
     let finalDescription;
@@ -16,9 +18,9 @@ const ProductListingCard = ({ product }) => {
     }
 
     return (
-        <div className="pt-16">
+        <div className="pt-16" onClick={() => navigate(`/shop/singleProduct/${_id}`)}>
             <div className="w-[615px] h-[320px] bg-[linear-gradient(104.63deg,#75204C_1.61%,_#360C41_97.81%)] rounded-[18px] px-[35px] py-[31px] relative shadow-[2px_2px_10px_2px_rgba(0,0,0,0.12)]">
-                <img src={img} alt="product" className="absolute -top-[20%] right-[20%] w-[330px] h-[231px]" />
+                <img loading="lazy" src={img} alt="product" className="absolute -top-[20%] right-[20%] w-[330px] h-[231px]" />
                 <div className="text-white flex flex-col items-start justify-end h-full font-playFair">
                     <p className="text-[14px] font-normal">BRAND</p>
                     <p className="text-[24px] font-normal">{name}</p>
