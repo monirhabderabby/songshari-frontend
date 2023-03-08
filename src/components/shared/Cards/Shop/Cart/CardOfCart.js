e1da8bd812834d7f8401da7c8aad93add30df9e1
Monir <monirhrabby.personal@gmail.com>
2023-03-08 20:27:57 +0600

    added cross button in cart

--- src/components/shared/Cards/Shop/Cart/CardOfCart.js
+++ src/components/shared/Cards/Shop/Cart/CardOfCart.js
@@ -1,13 +1,21 @@
+// Configuration
 import React from "react";
 import { useNavigate } from "react-router";
+
+// Third party packages
+import { RxCross2 } from "react-icons/rx";
 
 export const CardOfCart = ({ product, index }) => {
     const navigate = useNavigate();
     const { name, price, photos, _id } = product || {};
+
     return (
-        <div className="w-full h-auto lg:h-[229px] bg-[linear-gradient(166deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)] shadow-[0px_0px_4px_rgba(0,0,0,0.1), 0px_2px_4px_rgba(0,0,0,0.1)] rounded-[24px] px-[12px] lg:px-[36px]">
+        <div className="w-full h-auto lg:h-[229px] bg-[linear-gradient(166deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)] shadow-[0px_0px_4px_rgba(0,0,0,0.1), 0px_2px_4px_rgba(0,0,0,0.1)] rounded-[24px] px-[12px] lg:px-[36px] relative">
             <div className="w-[30px] h-[30px] lg:w-[40px] lg:h-[48px] bg-[#942DD9] rounded-[0px_0px_20px_20px] text-white text-bold font-Inter text-[10px] lg:text-[20px] flex justify-center items-center mb-[15px] lg:mb-0">
                 0{index + 1}
+            </div>
+            <div className="absolute right-[24px] top-[20px]">
+                <RxCross2 className="hover:text-white text-[20px] text-gray-300 duration-300" />
             </div>
             <div className="h-auto lg:h-[calc(229px-48px)] w-full">
                 <div className="flex h-full flex-col justify-between pb-[17px] w-full">
