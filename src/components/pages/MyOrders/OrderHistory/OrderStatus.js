6cb02b5217b2ecd989e46ceabc6838dd8f21e060
Monir <monirhrabby.personal@gmail.com>
2023-03-12 07:15:45 +0600

    added animation in order status card

--- src/components/pages/MyOrders/OrderHistory/OrderStatus.js
+++ src/components/pages/MyOrders/OrderHistory/OrderStatus.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { AllOrders } from "./AllOrders";
 import { StatusSelector } from "./StatusSelector";
 
 export const OrderStatus = () => {
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    }, []);
     return (
         <div className="flex flex-col gap-y-[20px] h-full ">
             <StatusSelector />
