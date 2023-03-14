c93d6f1181b523f7a214c8957472ab884c0beae7
Monir <monirhrabby.personal@gmail.com>
2023-03-14 08:33:42 +0600

    discount function

--- src/components/pages/Checkout/RightSideCheckoutContainer/DiscountForm/DiscountForm.js
+++ src/components/pages/Checkout/RightSideCheckoutContainer/DiscountForm/DiscountForm.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Third party package
 import TextField from "@mui/material/TextField";
+import { useSelector } from "react-redux";
 
 const DiscountForm = () => {
+    const [coupon, setCoupon] = useState("");
+    const { subTotal } = useSelector(state => state.persistedReducer?.billingSummary?.billingSummary) || {};
+
+    const onSubmit = () => {
+        const data = {
+            coupon,
+            subTotal,
+        };
+
+        console.log(data);
+    };
+
     return (
         <div className="mt-6 pb-6 flex items-center justify-between gap-x-[10px]">
-            <TextField id="outlined-basic" label="Discount Code" variant="outlined" size="small" className="w-full lg:w-[241px]" />
+            <TextField
+                id="outlined-basic"
+                label="Discount Code"
+                variant="outlined"
+                size="small"
+                className="w-full lg:w-[241px]"
+                onChange={e => setCoupon(e.target.value)}
+            />
             <button
-                className=" px-4 lg:px-8 rounded text-white text-[12px] lg:text-[16px] font-bold h-[40px]"
-                style={{
-                    backgroundImage: "linear-gradient(90deg, #E22987 0%, #A82BC5 100%)",
-                }}
+                className={`px-4 lg:px-8 rounded text-white text-[12px] lg:text-[16px] font-bold h-[40px] bg-[linear-gradient(90deg,_#E22987_0%,_#AB2BC5_100%)]`}
+                onClick={onSubmit}
             >
                 Apply
             </button>
