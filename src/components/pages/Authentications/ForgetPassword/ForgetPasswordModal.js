e14c8964221dddbe6acaa71559b86e27e318dbe9
Md Robin Hossain <hossainmdrobin967@gmail.com>
2023-03-14 17:21:20 +0600

    ss

--- src/components/pages/Authentications/ForgetPassword/ForgetPasswordModal.js
+++ src/components/pages/Authentications/ForgetPassword/ForgetPasswordModal.js
@@ -1,37 +1,29 @@
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import "../../../../App.css";
-import setCookie from "../../../../Helper/cookies/setCookie";
 import {
   useSendOtpByEmailMutation,
-  useChangePasswordMutation,
 } from "../../../../Redux/features/Verification.js/verification";
 import OtpInput from "./OtpInput";
 import PasswordInput from "./PasswordInput";
 import SendOtp from "./SendOtp";
 
 export default function ForgetPasswordModal({ setOpen }) {
+  const [otp, setOtp] = useState("");
+  const [submittedOtp, setSubmittedOtp] = useState("");
   const [email, setEmail] = useState("");
   const [sendOtpByEmail, { data, isLoading, error }] =
     useSendOtpByEmailMutation();
-  const [
-    changePassword,
-    { data: otpData, isLoading: passLoading, error: otpError },
-  ] = useChangePasswordMutation();
-
   useEffect(() => {
-    if (data) toast.success(data?.message);
+    if (data) {
+      toast.success(data?.message);
+      setOtp(data?.data?.otp);
+    };
     if (error) toast.error(error?.message);
-    if (otpError) toast.error(otpError?.message);
-  }, [data, error, otpError]);
-  useEffect(() => {
-    if (otpData) {
-      setCookie("token", otpData?.data?.token);
-    }
-  }, [otpData]);
+  }, [data, error]);
   return (
     <>
-      {!data && !otpData && (
+      {!data && !otp && (
         <SendOtp
           sendOtpByEmail={sendOtpByEmail}
           isLoading={isLoading}
@@ -40,19 +32,18 @@ export default function ForgetPasswordModal({ setOpen }) {
           setEmail={setEmail}
         />
       )}
-      {!otpData && data && (
+      {otp && data && (
         <OtpInput
-          changePassword={changePassword}
-          passLoading={passLoading}
           email={email}
+          otp={otp}
+          submittedOtp={submittedOtp}
+          setSubmittedOtp={setSubmittedOtp}
           setOpen={setOpen}
         />
       )}
-      {data && otpData && <PasswordInput setOpen={setOpen} />}
+      {data && otp === submittedOtp && (
+        <PasswordInput setOpen={setOpen} otp={otp} email={data?.data?.email} />
+      )}
     </>
   );
 }
-
-// className="flex-1 outline-none h-full text-sm bg-[linear-gradient(166deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)] w-full rounded-xl p-2 mt-3 text-white"
-
-// className="flex-1 outline-none h-full bg-transparent text-sm text-gray-400"
