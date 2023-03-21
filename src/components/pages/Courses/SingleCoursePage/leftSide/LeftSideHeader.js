ea8875b92195680d1b22715232174069f4e043f1
Monir <monirhrabby.personal@gmail.com>
2023-03-21 21:10:53 +0600

    fix height in course instructor compo

--- src/components/pages/Courses/SingleCoursePage/leftSide/LeftSideHeader.js
+++ src/components/pages/Courses/SingleCoursePage/leftSide/LeftSideHeader.js
@@ -6,7 +6,7 @@ export const LeftSideHeader = ({ course }) => {
         <div>
             <h1 className="text-[#354895] text-[34px] font-bold font-outfit">{name}</h1>
             <p className="text-[20px] text-gray-500 font-syne mt-[14px]">{description}</p>
-            <img src={image} alt="CourseImage" className="mt-[18px] rounded-[6px] max-w-[666px] max-h-[371px]" loading="lazy" />
+            <img src={image} alt="CourseImage" className="mt-[18px] w-full rounded-[6px] lg:max-w-[666px] lg:max-h-[371px]" loading="lazy" />
         </div>
     );
 };
