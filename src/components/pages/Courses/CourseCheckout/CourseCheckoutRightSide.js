e053313ee3b93820eb6f6844412c237743d8f002
Monir <monirhrabby.personal@gmail.com>
2023-03-23 06:19:17 +0600

    course buy api intregate

--- src/components/pages/Courses/CourseCheckout/CourseCheckoutRightSide.js
+++ src/components/pages/Courses/CourseCheckout/CourseCheckoutRightSide.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MdSecurity } from "react-icons/md";
+import { useBuyCourseMutation } from "../../../../Redux/features/Course/courseApi";
+import { OvalLoader } from "../../../shared/Cards/Loader/OvalLoader/OvalLoader";
+import Error from "../../../ui/error/Error";
 
 export const CourseCheckoutRightSide = ({ course }) => {
-    const { name, image, price } = course || {};
+    const [customError, setCustomError] = useState("");
+    const { name, image, price, _id } = course || {};
+
+    const [buyCourse, { isLoading, isSuccess, error, isError }] = useBuyCourseMutation();
+
+    const handleOrder = () => {
+        buyCourse({
+            id: _id,
+        });
+    };
+
+    useEffect(() => {
+        if (error) {
+            const { data } = error || {};
+            setCustomError(data?.message);
+        }
+    }, [error]);
     return (
         <div className="mt-[30px] w-full shadow-[0px_2px_6px_rgba(0,0,0,0.14)] p-[20px] rounded-[4px] bg-white">
             <h3 className="text-[20px] font-fira font-semibold text-black">Your order at a glance</h3>
@@ -26,9 +45,17 @@ export const CourseCheckoutRightSide = ({ course }) => {
                 <MdSecurity className="text-[14px] text-green-500" />
                 <p className="font-Inter text-[14px] text-green-500">Payment Secured</p>
             </div>
-
-            <button className="w-full mt-[20px] lg:mt-[30px] h-[40px] rounded-[4px] flex justify-center items-center bg-[linear-gradient(166deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)] hover:bg-[linear-gradient(166deg,rgb(255,0,128)_0%,rgb(148,45,255)_100%)] text-white">
-                Order Now
+            {isError && <Error message={customError} />}
+            <button
+                className={`w-full mt-[20px] lg:mt-[30px] h-[40px] rounded-[4px] flex justify-center items-center ${
+                    isSuccess ? "bg-green-400" : "bg-[linear-gradient(166deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)]"
+                } ${
+                    !isSuccess || (!isError && "hover:bg-[linear-gradient(166deg,rgb(255,0,128)_0%,rgb(148,45,255)_100%)]")
+                } text-white duration-300 ${isError && "cursor-not-allowed"}`}
+                onClick={handleOrder}
+                disabled={isSuccess || isError}
+            >
+                {isLoading ? <OvalLoader height={30} width={30} color="#FFFFFF" /> : isSuccess ? "Purchased" : <span>Purchase Now</span>}
             </button>
         </div>
     );
