68cc21be999102386a83fc490c4a2f42a921eba0
mobashirul-alam <mobashirulalamseam@gmail.com>
2023-04-01 19:54:29 +0600

    single package details almost done

--- src/components/pages/PackagePage/SinglePackageDetailsPage/SinglePackageDetailsPage.js
+++ src/components/pages/PackagePage/SinglePackageDetailsPage/SinglePackageDetailsPage.js
@@ -1,13 +1,11 @@
 // Configuration
 import React, { useEffect } from "react";
-import { useParams } from "react-router";
 
 // Components
 import CustomHeader from "../../../shared/CustomHeader/CustomHeader";
+import SinglePackageLeft from "./SinglePackage/SinglePackageLeft/SinglePackageLeft";
 
 const SinglePackageDetailsPage = () => {
-  const { id } = useParams();
-
   // Scroll to top
   useEffect(() => {
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
@@ -15,8 +13,16 @@ const SinglePackageDetailsPage = () => {
   return (
     <div>
       <CustomHeader title={"Package"} />
-      <div className="lg:max-w-[1000px] xl:max-w-[1200px] mx-auto">
+      {/* <div className="lg:max-w-[1000px] xl:max-w-[1200px] mx-auto">
         <h1>This is package single page id - {id}</h1>
+      </div> */}
+      <div className="w-full lg:w-[1200px]  2xl:w-[1400px] mx-auto h-auto flex lg:flex-row flex-col gap-y-[20px] pt-[44px] px-[26px] mb-12">
+        <div className=" w-full lg:max-w-[660px] 2xl:w-[850px] mx-auto">
+          <SinglePackageLeft />
+        </div>
+        <div className="sticky top-[44px] flex-1 mx-auto max-w-[400px]">
+          {/* <SingleCourseInfoCard {...{ course }} /> */}Prize Info
+        </div>
       </div>
     </div>
   );
