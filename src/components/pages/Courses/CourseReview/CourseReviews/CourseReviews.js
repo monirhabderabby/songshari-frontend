a0edff5ce62b26c8be706cc27e451f2c7e8d5a56
Monir <monirhrabby.personal@gmail.com>
2023-04-03 08:47:42 +0600

    added pagination in course reviews

--- src/components/pages/Courses/CourseReview/CourseReviews/CourseReviews.js
+++ src/components/pages/Courses/CourseReview/CourseReviews/CourseReviews.js
@@ -1,4 +1,5 @@
-import React from "react";
+import { Pagination } from "@mui/material";
+import React, { useState } from "react";
 import { useParams } from "react-router";
 import { useGetAllReviewsByCourseIDQuery } from "../../../../../Redux/features/Course/courseApi";
 import { LineWaveLoader } from "../../../../shared/Cards/Loader/lineWaveLoader/LineWaveLoader";
@@ -6,46 +7,57 @@ import { ServerErrorMessage } from "../../../../ui/error/ServerErrorMessage";
 import { CourseCard } from "./CourseCard";
 
 export const CourseReviews = () => {
-  const { id } = useParams();
-  const { isLoading, data, isError } = useGetAllReviewsByCourseIDQuery(id);
+    const [page, setPage] = useState(1);
+    const { id } = useParams();
+    const { isLoading, isFetching, data, isError } = useGetAllReviewsByCourseIDQuery({ id: id, page: page, limit: 3 });
 
-  const { reviews } = data || {};
+    const { reviews, total } = data || {};
+    const paginationPage = Math.ceil(total / 3);
 
-  let content;
-  if (isLoading) {
-    content = (
-      <div className="h-[250px] w-full flex justify-center items-center">
-        <LineWaveLoader />
-      </div>
-    );
-  } else if (!isLoading && isError) {
-    content = (
-      <div className="h-[250px] w-full flex justify-center items-center">
-        <ServerErrorMessage />
-      </div>
-    );
-  } else if (!isLoading && reviews?.length === 0) {
-    content = (
-      <div className="h-[200px] w-full flex justify-center items-center text-[18px] font-Nunito text-gray-500">
-        No Reviews Found!
-      </div>
-    );
-  } else if (!isLoading && reviews?.length > 0) {
-    content = (
-      <div className="w-full flex flex-col gap-y-[30px]">
-        {reviews?.map((item) => {
-          return <CourseCard key={item?._id} review={item} />;
-        })}
-      </div>
-    );
-  }
+    let content;
+    if (isLoading || isFetching) {
+        content = (
+            <div className="h-[200px] w-full flex justify-center items-center">
+                <LineWaveLoader />
+            </div>
+        );
+    } else if (!isLoading && isError) {
+        content = (
+            <div className="h-[250px] w-full flex justify-center items-center">
+                <ServerErrorMessage />
+            </div>
+        );
+    } else if (!isLoading && reviews?.length === 0) {
+        content = <div className="h-[200px] w-full flex justify-center items-center text-[18px] font-Nunito text-gray-500">No Reviews Found!</div>;
+    } else if (!isLoading && reviews?.length > 0) {
+        content = (
+            <div className="w-full flex flex-col gap-y-[30px]">
+                {reviews?.map(item => {
+                    return <CourseCard key={item?._id} review={item} />;
+                })}
+            </div>
+        );
+    }
 
-  return (
-    <div className="my-[30px] lg:my-[50px]">
-      <h2 className="text-[32px] text-gray-700 font-semibold mb-[20px] border-b-2">
-        Reviews ({reviews?.length})
-      </h2>
-      {content}
-    </div>
-  );
+    return (
+        <div className="my-[30px] lg:my-[50px]">
+            <h2 className="text-[32px] text-gray-700 font-semibold mb-[20px] border-b-2">Reviews ({reviews?.length})</h2>
+            {content}
+            <div className="mt-[50px] w-full flex justify-end">
+                <Pagination
+                    count={paginationPage}
+                    onChange={(e, value) => setPage(value)}
+                    sx={{
+                        "& .MuiPaginationItem-root.Mui-selected": {
+                            backgroundImage: "linear-gradient(166deg, rgb(255,0,128) 0%, rgb(148,45,255) 100%)",
+                            color: "#FFFFFF",
+                        },
+                        "& .MuiPaginationItem-root.Mui-selected:hover": {
+                            backgroundImage: "linear-gradient(166deg, rgb(255,0,128) 25%, rgb(148,45,255) 100%)",
+                        },
+                    }}
+                />
+            </div>
+        </div>
+    );
 };
