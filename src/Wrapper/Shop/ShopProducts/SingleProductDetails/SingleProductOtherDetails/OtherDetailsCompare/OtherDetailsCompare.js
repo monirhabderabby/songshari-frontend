21f97776e2c387fa90e63ba45e9bf2d9bb8273b3
mobashirul-alam <mobashirulalamseam@gmail.com>
2023-04-04 12:56:48 +0600

    update

--- src/Wrapper/Shop/ShopProducts/SingleProductDetails/SingleProductOtherDetails/OtherDetailsCompare/OtherDetailsCompare.js
+++ src/Wrapper/Shop/ShopProducts/SingleProductDetails/SingleProductOtherDetails/OtherDetailsCompare/OtherDetailsCompare.js
@@ -2,28 +2,32 @@ import React from "react";
 import OtherDetailsCompareCard from "./OtherDetailsCompareCard/OtherDetailsCompareCard";
 
 const OtherDetailsCompare = ({ data, similarProducts }) => {
-    const { products } = similarProducts || {};
+  const { products } = similarProducts || {};
 
-    let content;
-    if (products?.length === 0) {
-        content = (
-            <div className="h-[100px] w-full flex justify-center items-center text-[18px] text-gray-400 font-outfit">No Similer product found!</div>
-        );
-    } else {
-        content = (
-            <div className="grid grid-cols-2 gap-9">
-                {products?.map(product => (
-                    <OtherDetailsCompareCard key={product._id} {...{ product }} />
-                ))}
-            </div>
-        );
-    }
-    return (
-        <div className="mt-12">
-            <h1 className="text-[#272727] text-3xl leading-[34px] font-semibold mb-10">Similar Products</h1>
-            {content}
-        </div>
+  let content;
+  if (products?.length === 0) {
+    content = (
+      <div className="h-[100px] w-full flex justify-center items-center text-[18px] text-gray-400 font-outfit">
+        No Similar product found!
+      </div>
     );
+  } else {
+    content = (
+      <div className="grid grid-cols-2 gap-9">
+        {products?.map((product) => (
+          <OtherDetailsCompareCard key={product._id} {...{ product }} />
+        ))}
+      </div>
+    );
+  }
+  return (
+    <div className="mt-12">
+      <h1 className="text-[#272727] text-3xl leading-[34px] font-semibold mb-10">
+        Similar Products
+      </h1>
+      {content}
+    </div>
+  );
 };
 
 export default OtherDetailsCompare;
--- src/components/pages/Courses/LandingPage/RightSideCourses/RightSideCourses.js
+++ src/components/pages/Courses/LandingPage/RightSideCourses/RightSideCourses.js
@@ -11,48 +11,58 @@ import { ServerErrorMessage } from "../../../../ui/error/ServerErrorMessage";
 import { CoursesCountBox } from "./CoursesCountBox";
 
 export const RightSideCourses = () => {
-    const [page, setPage] = useState(1);
-    const { types, skillLevel, category } = useSelector(state => state.persistedReducer.course.filter) || {};
+  const [page, setPage] = useState(1);
+  const { types, skillLevel, category } =
+    useSelector((state) => state.persistedReducer.course.filter) || {};
 
-    // get data by filtering
-    const { data, isLoading, isError, isFetching } = useGetAllCoursesQuery({
-        page: page,
-        limit: 9,
-        categroyId: category,
-        typeId: types,
-        skill: skillLevel,
-    });
+  // get data by filtering
+  const { data, isLoading, isError, isFetching } = useGetAllCoursesQuery({
+    page: page,
+    limit: 9,
+    categroyId: category,
+    typeId: types,
+    skill: skillLevel,
+  });
 
-    const { count, total, courses } = data || {};
+  const { count, total, courses } = data || {};
 
-    const pageCounting = Math.ceil(total / 9);
+  const pageCounting = Math.ceil(total / 9);
 
-    // Get all courses
-    let content;
-    if (isLoading || isFetching) {
-        content = <LineWaveLoader />;
-    } else if (!isLoading && isError) {
-        content = <ServerErrorMessage />;
-    } else if (!isLoading && courses?.length === 0) {
-        content = <div className="w-full h-[350px] flex justify-center items-center font-Inter text-gray-500 text-[20px]">No Course Found!</div>;
-    } else if (!isLoading && courses?.length > 0) {
-        content = (
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-[20px]">
-                {courses?.map(item => {
-                    return <CourseCard key={item?._id} course={item} />;
-                })}
-            </div>
-        );
-    }
-    return (
-        <div className="flex flex-col w-full">
-            <CoursesCountBox {...{ count, total, page }} />
-            <div>{content}</div>
-            {total > 8 && (
-                <div className="my-[100px] w-full flex justify-center">
-                    <Pagination count={pageCounting} variant="outlined" color="secondary" onChange={(event, value) => setPage(value)} />
-                </div>
-            )}
-        </div>
+  // Get all courses
+  let content;
+  if (isLoading || isFetching) {
+    content = <LineWaveLoader />;
+  } else if (!isLoading && isError) {
+    content = <ServerErrorMessage />;
+  } else if (!isLoading && courses?.length === 0) {
+    content = (
+      <div className="w-full h-[350px] flex justify-center items-center font-Inter text-gray-500 text-[20px]">
+        No Course Found!
+      </div>
     );
+  } else if (!isLoading && courses?.length > 0) {
+    content = (
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-[20px]">
+        {courses?.map((item) => {
+          return <CourseCard key={item?._id} course={item} />;
+        })}
+      </div>
+    );
+  }
+  return (
+    <div className="flex flex-col w-full">
+      <CoursesCountBox {...{ count, total, page }} />
+      <div>{content}</div>
+      {total > 8 && (
+        <div className="my-[100px] w-full flex justify-center">
+          <Pagination
+            count={pageCounting}
+            variant="outlined"
+            color="secondary"
+            onChange={(event, value) => setPage(value)}
+          />
+        </div>
+      )}
+    </div>
+  );
 };
