7b96c22656d0ab96262c167d124012ba74e5ae16
Monir <monirhrabby.personal@gmail.com>
2023-04-09 10:25:04 +0600

    developed refferal withdraw page

--- src/components/pages/MyOrders/Refferral/Withdraw/Header/RefWithdrawHeader.js
+++ src/components/pages/MyOrders/Refferral/Withdraw/Header/RefWithdrawHeader.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
+import { RefferalWithdrawModal } from "./RefferalWithdrawModal";
 
 export const RefWithdrawHeader = () => {
+    const [open, setOpen] = useState(false);
+
+    const modalControll = () => {
+        setOpen(!open);
+    };
     return (
-        <div className="w-full flex justify-evenly gap-x-[10px]">
-            <div className="bg-[linear-gradient(315deg,#eb4786_0%,#b854ab_74%)] w-full lg:w-[300px] min-h-[130px] h-auto flex flex-col items-center justify-evenly rounded-[4px]">
-                <span className="text-white text-[22px] font-Inter font-semibold">200</span>
-                <span className="text-gray-300">Referral Points</span>
-            </div>
-            <div className="bg-[rgba(37,188,241,0.15)] w-full lg:w-[300px] min-h-[130px] h-auto flex flex-col items-center justify-evenly rounded-[4px] hover:shadow-md duration-300 cursor-pointer">
-                <div className="h-[50px] w-[50px] bg-orange-400 flex justify-center items-center rounded-full">
-                    <AiOutlinePlus className="text-white text-[22px]" />
+        <>
+            <div className="w-full flex justify-evenly gap-x-[10px]">
+                <div className="bg-[linear-gradient(315deg,#eb4786_0%,#b854ab_74%)] w-full lg:w-[300px] min-h-[130px] h-auto flex flex-col items-center justify-evenly rounded-[4px]">
+                    <span className="text-white text-[22px] font-Inter font-semibold">200</span>
+                    <span className="text-gray-300">Referral Points</span>
                 </div>
-                <span className="text-red-500 font-semibold">Withdraw Request</span>
+                <div
+                    className="bg-[rgba(37,188,241,0.15)] w-full lg:w-[300px] min-h-[130px] h-auto flex flex-col items-center justify-evenly rounded-[4px] hover:shadow-md duration-300 cursor-pointer"
+                    onClick={modalControll}
+                >
+                    <div className="h-[50px] w-[50px] bg-orange-400 flex justify-center items-center rounded-full">
+                        <AiOutlinePlus className="text-white text-[22px]" />
+                    </div>
+                    <span className="text-red-500 font-semibold">Withdraw Request</span>
+                </div>
             </div>
-        </div>
+            {open && <RefferalWithdrawModal modalControll={modalControll} />}
+        </>
     );
 };
---
+++ src/components/pages/MyOrders/Refferral/Withdraw/Header/RefferalWithdrawModal.js
@@ -0,0 +1,24 @@
+import { Input } from "antd";
+import React from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import { Modal } from "../../../../../modals/Modal";
+
+const { TextArea } = Input;
+
+export const RefferalWithdrawModal = ({ modalControll }) => {
+    return (
+        <Modal modalControll={modalControll}>
+            <div className="w-full flex justify-between items-center border-b-[1px] border-gray-200 pb-[10px]">
+                <h3 className="text-[22px] font-outfit font-medium">Wallet Withdraw Request</h3>
+                <AiOutlineClose className="text-[24px] cursor-pointer text-gray-500 hover:text-black" onClick={modalControll} />
+            </div>
+            <div className="flex flex-col gap-y-[15px]">
+                <Input placeholder="Amount" size="large" />
+                <TextArea rows={4} placeholder="Write a notes" />
+                <button className="bg-[linear-gradient(315deg,#eb4786_0%,#b854ab_74%)] h-[40px] rounded-[8px] text-white font-Inter font-medium">
+                    Confirm
+                </button>
+            </div>
+        </Modal>
+    );
+};
