ded9b836e1754173f315bda2e2adacb5210f887c
Monir <monirhrabby.personal@gmail.com>
2023-04-11 03:47:33 +0600

    a

--- src/components/pages/MyOrders/Refferral/RefferralContainer.js
+++ src/components/pages/MyOrders/Refferral/RefferralContainer.js
@@ -1,7 +1,6 @@
 import { Tabs } from "antd";
 import React from "react";
 import { AllRefferedContainer } from "./AllReffered/AllRefferedContainer";
-import { ReferralEarnings } from "./Earnings/ReferralEarnings";
 import { WithDrawContainer } from "./Withdraw/WithDrawContainer";
 
 export const RefferralContainer = () => {
@@ -16,11 +15,6 @@ export const RefferralContainer = () => {
         },
         {
             key: "2",
-            label: `Earnings`,
-            children: <ReferralEarnings />,
-        },
-        {
-            key: "3",
             label: `WithDraw`,
             children: <WithDrawContainer />,
         },
