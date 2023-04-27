248e084874540d9cb13a801b9c097dbea020c535
mobashirul-alam <mobashirulalamseam@gmail.com>
2023-04-27 17:15:12 +0600

    course code removed from matrimony

--- src/route/Course/courseRoute.js
+++ src/route/Course/courseRoute.js
@@ -1,4 +1,3 @@
-import { CourseCheckout } from "../../components/pages/Courses/CourseCheckout/CourseCheckout";
 import { CourseLandingPage } from "../../components/pages/Courses/LandingPage/CourseLandingPage";
 
 const courseRoute = [
@@ -6,11 +5,6 @@ const courseRoute = [
     id: 1,
     path: "/courses",
     element: <CourseLandingPage />,
-  },
-  {
-    id: 3,
-    path: "/course/checkout/:id",
-    element: <CourseCheckout />,
   },
 ];
 
