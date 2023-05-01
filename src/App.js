06dde7b451dd5e855f41c16f2400201a9a7bc9eb
mobashirul-alam <mobashirulalamseam@gmail.com>
2023-05-01 20:10:13 +0600

    env file edited

--- src/App.js
+++ src/App.js
@@ -7,10 +7,8 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import MasterRoute from "./route/MasterRoute";
-import { shopBaseUrl } from "./config";
 
 function App() {
-  //   alert("Shop Base Url=" + shopBaseUrl);
   return <MasterRoute />;
 }
 
