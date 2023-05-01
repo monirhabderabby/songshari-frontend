3619431a6c2b5922e513ae951991846232a130f2
hrridoyv2 <hrridoyv2@gmail.com>
2023-05-01 17:19:21 +0600

    dynamic base url

--- src/config/index.js
+++ src/config/index.js
@@ -5,3 +5,5 @@ export const apiBaseUrl =
         ? process.env.REACT_APP_API_BASE_URL_TEST
         : process.env.REACT_APP_API_BASE_URL;
 export const apiVersionPrefix = process.env.REACT_APP_API_VERSION;
+
+export const shopBaseUrl = process.env.REACT_APP_ENV === "local"? process.env.REACT_APP_SHOP_LOCAL_BASE_URL: process.env.REACT_APP_SHOP_LIVE_BASE_URL 
