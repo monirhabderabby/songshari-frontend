466c5d6356f9e930c38cfbb9784f379a6199020b
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2023-05-06 08:27:07 +0600

    added: verification system

--- src/components/pages/MyOrders/Account.js
+++ src/components/pages/MyOrders/Account.js
@@ -3,16 +3,19 @@ import React from "react";
 import { Toaster } from "react-hot-toast";
 import ChangeEmail from "./accountConponents/ChangeEmail";
 import ChangePassword from "./accountConponents/ChangePassword";
+import { Verification } from "./accountConponents/Verification";
 
 export const Account = () => {
-  
-  return (
-    <div className="w-full bg-white">
-      <ChangeEmail />
-      <div className="my-20">
-        <ChangePassword />
-      </div>
-      <Toaster />
-    </div>
-  );
+    return (
+        <div className="w-full bg-white">
+            <Verification />
+            <div className="my-12">
+                <ChangeEmail />
+            </div>
+            <div className="my-12">
+                <ChangePassword />
+            </div>
+            <Toaster />
+        </div>
+    );
 };
