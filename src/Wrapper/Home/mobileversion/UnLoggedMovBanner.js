be0840f95444a250b91f6c5eb110f9f3ee4886d3
Md Robin Hossain <hossainmdrobin967@gmail.com>
2023-05-15 14:12:46 +0600

    bug fixed

---
+++ src/Wrapper/Home/mobileversion/UnLoggedMovBanner.js
@@ -0,0 +1,24 @@
+
+
+const UnLoggedMovBanner = () => {
+
+  return (
+    <>
+      <section className="bg-[url('https://img1.picmix.com/output/stamp/normal/9/9/4/3/1503499_8efaf.gif')] w-full py-4 flex items-center pt-4">
+        <div className="max-w-[1200px] w-full mx-auto">
+          <div className="h-full w-full flex items-center px-4 gap-x-3">
+            <img
+              className="h-auto max-w-[611px] w-full rounded-[12px]"
+              style={{ boxShadow: "2px 2px 10px rgba(0, 0, 0, 0.12)" }}
+              src="https://images.pexels.com/photos/1779414/pexels-photo-1779414.jpeg?auto=compress&cs=tinysrgb&w=800"
+              alt="Not Available"
+            />
+            
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default UnLoggedMovBanner;
