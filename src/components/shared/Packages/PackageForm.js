c4aadb73e3e1a950cd040686c3a68ea07de3af92
Md Robin Hossain <hossainmdrobin967@gmail.com>
2023-05-22 17:51:38 +0600

    done

--- src/components/shared/Packages/PackageForm.js
+++ src/components/shared/Packages/PackageForm.js
@@ -7,6 +7,7 @@ import {
 } from "../../../Redux/features/package/packageApi";
 import { SuccessSnackBar } from "../../ui/error/snackBar/SuccessSnackBar";
 import Error from "../../ui/error/Error";
+import PaymentSelector from "./PaymentSelector";
 
 const PackageForm = ({ setShowPopup, pack, useCase, setSelectedPack }) => {
   const [successSnackBarOpen, setSuccessSnackBarOpen] = useState(false);
@@ -14,10 +15,6 @@ const PackageForm = ({ setShowPopup, pack, useCase, setSelectedPack }) => {
   const [selectedOption, setSelectedOption] = useState("30");
   const [isChecked, setIsChecked] = useState(false);
   const [customError, setCustomError] = useState("");
-
-  const handleCheckboxChange = () => {
-    setIsChecked(!isChecked);
-  };
 
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
@@ -108,75 +105,68 @@ const PackageForm = ({ setShowPopup, pack, useCase, setSelectedPack }) => {
       ></div>
       <div className="rounded-xl sm:max-w-full bg-[linear-gradient(166deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)] w-[400px] space-y-4 bg-white p-6 fixed top-1/2 left-1/2 z-20 -translate-x-1/2 -translate-y-1/2">
         <h3 className="text-white text-xl font-bold">Purchase Package for:</h3>
-        <div>
-          <label className="text-white inline-flex items-center">
-            <input
-              type="radio"
-              className="form-radio h-5 w-5 text-blue-600"
-              value="30"
-              checked={selectedOption === "30"}
-              onChange={handleOptionChange}
-            />
-            <span className="ml-2 text-white">1 Month</span>
-          </label>
+        <div className="grid grid-cols-2 md:grid-cols-1">
+          <div className="ml-4">
+            <label className="text-white inline-flex items-center">
+              <input
+                type="radio"
+                className="form-radio h-5 w-5 text-blue-600"
+                value="30"
+                checked={selectedOption === "30"}
+                onChange={handleOptionChange}
+              />
+              <span className="ml-2 text-white">1 Month</span>
+            </label>
+          </div>
+          <div className="ml-4">
+            <label className="inline-flex items-center">
+              <input
+                type="radio"
+                className="form-radio h-5 w-5 text-blue-600"
+                value="90"
+                checked={selectedOption === "90"}
+                onChange={handleOptionChange}
+              />
+              <span className="ml-2 text-white">3 Month</span>
+            </label>
+          </div>
+          <div className="ml-4">
+            <label className="inline-flex items-center">
+              <input
+                type="radio"
+                className="form-radio h-5 w-5 text-blue-600"
+                value="180"
+                checked={selectedOption === "180"}
+                onChange={handleOptionChange}
+              />
+              <span className="ml-2 text-white">6 Month</span>
+            </label>
+          </div>
+          <div className="ml-4">
+            <label className="inline-flex items-center">
+              <input
+                type="radio"
+                className="form-radio h-5 w-5 text-blue-600"
+                value="365"
+                checked={selectedOption === "365"}
+                onChange={handleOptionChange}
+              />
+              <span className="ml-2 text-white">1 Year</span>
+            </label>
+          </div>
         </div>
-        <div>
-          <label className="inline-flex items-center">
-            <input
-              type="radio"
-              className="form-radio h-5 w-5 text-blue-600"
-              value="90"
-              checked={selectedOption === "90"}
-              onChange={handleOptionChange}
-            />
-            <span className="ml-2 text-white">3 Month</span>
-          </label>
-        </div>
-        <div>
-          <label className="inline-flex items-center">
-            <input
-              type="radio"
-              className="form-radio h-5 w-5 text-blue-600"
-              value="180"
-              checked={selectedOption === "180"}
-              onChange={handleOptionChange}
-            />
-            <span className="ml-2 text-white">6 Month</span>
-          </label>
-        </div>
-        <div>
-          <label className="inline-flex items-center">
-            <input
-              type="radio"
-              className="form-radio h-5 w-5 text-blue-600"
-              value="365"
-              checked={selectedOption === "365"}
-              onChange={handleOptionChange}
-            />
-            <span className="ml-2 text-white">1 Year</span>
-          </label>
-        </div>
-
+        <PaymentSelector {...{ isChecked, setIsChecked }} />
         <p className="font-bold text-xl text-white">
           Package cost: <span className="text-2xl font-bold">{total} </span>
-          {isChecked?"POINT": "BDT"}
+          {isChecked ? "POINT" : "BDT"}
         </p>
 
         <button
           onClick={handleSubmit}
           className="bg-white font-bold text-lg px-4 py-2 rounded hover:bg-gray-100"
         >
-          {(isLoading || pointLoading) ? "Loading" : "Submit"}
+          {isLoading || pointLoading ? "Loading" : "Submit"}
         </button>
-        <label className="flex items-center space-x-2">
-          <input
-            type="checkbox"
-            className="form-checkbox h-5 w-5 text-indigo-600"
-            value={isChecked}
-            onChange={handleCheckboxChange}
-          />
-          <span className="text-white font-bold">Buy with SHONSHARI wallet</span>
-        </label>
         {customError && <Error message={customError} />}
       </div>
       <SuccessSnackBar
