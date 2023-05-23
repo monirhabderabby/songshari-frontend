a6f127a5c0603a709bebbae44c26904102c20c4a
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2023-05-23 18:20:54 +0600

    stun servers

--- src/components/pages/VideoCall/Test.jsx
+++ src/components/pages/VideoCall/Test.jsx
@@ -4,17 +4,39 @@ const Test = () => {
     const localVideoRef = useRef(null);
     const remoteVideoRef = useRef(null);
     let localStream;
+    let remoteStream;
     let peerConnection;
+
+    let constraints = {
+        video: {
+            width: { min: 640, ideal: 1920, max: 1920 },
+            height: { min: 480, ideal: 1080, max: 1080 },
+        },
+        audio: true,
+    };
+
+    let token = null;
+
+    const servers = {
+        iceServers: [
+            {
+                urls: ["stun:stun.1.google.com:19302", "stun:stun2.1.google.com:19302"],
+            },
+        ],
+    };
 
     useEffect(() => {
         const startVideoChat = async () => {
             try {
                 // Get local video stream
-                localStream = await navigator.mediaDevices.getUserMedia({ video: false, audio: true });
+                localStream = await navigator.mediaDevices.getUserMedia(constraints);
                 localVideoRef.current.srcObject = localStream;
 
                 // Create peer connection
-                peerConnection = new RTCPeerConnection();
+                peerConnection = new RTCPeerConnection(servers);
+
+                remoteStream = new MediaStream();
+                remoteVideoRef.current.srcObject = remoteStream;
 
                 // Add local stream to peer connection
                 localStream.getTracks().forEach(track => {
@@ -25,12 +47,15 @@ const Test = () => {
 
                 // Handle remote stream
                 peerConnection.ontrack = event => {
-                    const remoteStream = event.streams[0];
-                    remoteVideoRef.current.srcObject = remoteStream;
+                    event.streams[0].getTracks().forEach(track => {
+                        remoteStream.addTrack(track);
+                    });
+                    // const remoteStream = event.streams[0]
+                    // remoteVideoRef.current.srcObject = remoteStream;
                 };
 
                 // Handle ICE candidate
-                peerConnection.onicecandidate = event => {
+                peerConnection.onicecandidate = async event => {
                     if (event.candidate) {
                         // Send the ICE candidate to the remote peer
                     }
@@ -42,6 +67,7 @@ const Test = () => {
                         // Create offer
                         const offer = await peerConnection.createOffer();
                         await peerConnection.setLocalDescription(offer);
+                        console.log("offer: ", offer);
 
                         // Send the offer to the remote peer
                     } catch (error) {
@@ -89,9 +115,9 @@ const Test = () => {
     }, []);
 
     return (
-        <div>
-            <video ref={localVideoRef} autoPlay muted />
-            <video ref={remoteVideoRef} autoPlay />
+        <div className="grid grid-cols-2 overflow-hidden h-screen">
+            <video ref={localVideoRef} autoPlay playsInline className="bg-slate-700 w-full h-full object-cover" />
+            <video ref={remoteVideoRef} autoPlay playsInline className="bg-black w-full h-full object-cover" />
         </div>
     );
 };
