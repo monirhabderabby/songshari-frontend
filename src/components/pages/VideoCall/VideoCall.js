85cf486357c9a70ba048bc09791705420febf36c
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2023-05-23 08:44:54 +0600

    a

--- src/components/pages/VideoCall/VideoCall.js
+++ src/components/pages/VideoCall/VideoCall.js
@@ -1,81 +1,80 @@
 import React, { useEffect, useRef, useState } from "react";
 // import Peer from "simple-peer";
 // import copy from "clipboard-copy";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import AudioAvatar from "./AudioAvatar";
 import MainButtons from "./MainButtons";
-import TopRightButtons from "./TopRightButtons";
 import MovableComponent from "./MovableComponent";
 import TopLeftIntro from "./TopLeftIntro";
-import AudioAvatar from "./AudioAvatar";
+import TopRightButtons from "./TopRightButtons";
 // import MyMobileVideo from "./MyMobileVideo";
 
-const bgColor =
-  "bg-[linear-gradient(166deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)]";
+const bgColor = "bg-[linear-gradient(166deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)]";
 const VideoCall = () => {
-  const [showBottomRightVideo, setShowBottomRightVideo] = useState(true);
-  const [video, setVideo] = useState(true);
-  const [audio, setAudio] = useState(true);
-  // const [peer, setPeer] = useState(null);
-  // const [stream, setStream] = useState(null);
-  // const [otherId, setOtherId] = useState("");
-  // const [isCopied, setIsCopied] = useState(false);
-  const videoRef = useRef(null);
-  const myVideoRef = useRef(null);
-  const myMobileVideoRef = useRef(null);
-  // const
-  // const inputRef = useRef(null);
+    const [showBottomRightVideo, setShowBottomRightVideo] = useState(true);
+    const [video, setVideo] = useState(true);
+    const [audio, setAudio] = useState(true);
+    // const [peer, setPeer] = useState(null);
+    // const [stream, setStream] = useState(null);
+    // const [otherId, setOtherId] = useState("");
+    // const [isCopied, setIsCopied] = useState(false);
+    const videoRef = useRef(null);
+    const myVideoRef = useRef(null);
+    const myMobileVideoRef = useRef(null);
+    // const
+    // const inputRef = useRef(null);
 
-  useEffect(() => {
-    navigator.mediaDevices
-      .getUserMedia({ video: video, audio: audio })
-      .then((stream) => {
-        // setStream(stream);
-        videoRef.current.srcObject = stream;
-        myVideoRef.current.srcObject = stream;
-        myMobileVideoRef.current.srcObject = stream;
-      })
-      .catch((err) => console.log(err));
-  }, [video, audio]);
+    useEffect(() => {
+        navigator.mediaDevices
+            .getUserMedia({ video: video, audio: audio })
+            .then(stream => {
+                // setStream(stream);
+                videoRef.current.srcObject = stream;
+                myVideoRef.current.srcObject = stream;
+                myMobileVideoRef.current.srcObject = stream;
+            })
+            .catch(err => console.log(err));
+    }, [video, audio]);
 
-  // const handleCopy = () => {
-  //   const { current } = inputRef;
-  //   copy(current.value);
-  //   setIsCopied(true);
-  //   setTimeout(() => setIsCopied(false), 2000);
-  // };
+    // const handleCopy = () => {
+    //   const { current } = inputRef;
+    //   copy(current.value);
+    //   setIsCopied(true);
+    //   setTimeout(() => setIsCopied(false), 2000);
+    // };
 
-  // const handleConnect = () => {
-  //   const p = new Peer({ initiator: true, trickle: false, stream });
-  //   p.on("signal", (data) => {
-  //     setOtherId(JSON.stringify(data));
-  //   });
-  //   p.on("stream", (stream) => {
-  //     videoRef.current.srcObject = stream;
-  //   });
-  //   setPeer(p);
-  // };
+    // const handleConnect = () => {
+    //   const p = new Peer({ initiator: true, trickle: false, stream });
+    //   p.on("signal", (data) => {
+    //     setOtherId(JSON.stringify(data));
+    //   });
+    //   p.on("stream", (stream) => {
+    //     videoRef.current.srcObject = stream;
+    //   });
+    //   setPeer(p);
+    // };
 
-  // const handleOtherIdChange = (e) => {
-  //   setOtherId(e.target.value);
-  // };
+    // const handleOtherIdChange = (e) => {
+    //   setOtherId(e.target.value);
+    // };
 
-  // const handleJoin = () => {
-  //   const p = new Peer({ initiator: false, trickle: false, stream });
-  //   p.on("signal", (data) => {
-  //     peer.signal(JSON.stringify(data));
-  //   });
-  //   p.on("stream", (stream) => {
-  //     videoRef.current.srcObject = stream;
-  //   });
-  //   setPeer(p);
-  // };
+    // const handleJoin = () => {
+    //   const p = new Peer({ initiator: false, trickle: false, stream });
+    //   p.on("signal", (data) => {
+    //     peer.signal(JSON.stringify(data));
+    //   });
+    //   p.on("stream", (stream) => {
+    //     videoRef.current.srcObject = stream;
+    //   });
+    //   setPeer(p);
+    // };
 
-  return (
-    <div className=" overflow-hidden flex relative flex-col items-center justify-center w-screen h-screen bg-[linear-gradient(166deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)]">
-      <div className="flex flex-col items-center justify-center">
-        {video && <video muted className="w-screen h-screen" ref={videoRef} autoPlay />}
-        {/* <div className="mt-2 absolute bottom-0">
+    return (
+        <div className=" overflow-hidden flex relative flex-col items-center justify-center w-screen h-screen bg-[linear-gradient(166deg,rgb(242,40,118)_0%,rgb(148,45,217)_100%)]">
+            <div className="flex flex-col items-center justify-center">
+                {video && <video muted className="w-screen h-screen" ref={videoRef} autoPlay />}
+                {/* <div className="mt-2 absolute bottom-0">
           <button
             className="px-4 py-2 mr-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
             onClick={handleConnect}
@@ -99,28 +98,17 @@ const VideoCall = () => {
             Join
           </button>
         </div> */}
-      </div>
-      {!video && <AudioAvatar />}
-      <div className={`hidden md:block absolute bottom-3 ${showBottomRightVideo ? "right-2" : "-right-44"}`}>
-        <div className={`relative`}>
-          <video
-            className=" w-auto h-36 border-1 border-gray-200 rounded-lg shadow-md"
-            ref={myVideoRef}
-            autoPlay
-          />
-          <button
-            onClick={() => setShowBottomRightVideo(!showBottomRightVideo)}
-            className="backdrop-blur-lg w-8 h-8 p-2 flex justify-center items-center rounded-full  absolute left-0 top-[50%] bottom-[50%]"
-          >
-            {showBottomRightVideo ? (
-              <ArrowForwardIosIcon style={{ color: "white" }} />
-            ) : (
-              <ArrowBackIosIcon style={{ color: "white" }} />
-            )}
-          </button>
-        </div>
-      </div>
-      {/* <div className="flex flex-col items-center absolute bottom-10">
+            </div>
+            {!video && <AudioAvatar />}
+            <div className={`hidden md:block absolute bottom-3 ${showBottomRightVideo ? "right-2" : "-right-[250px]"}`}>
+                <div className={`relative`}>
+                    <video className=" w-auto bg-slate-900 h-36 border-1 border-gray-200 rounded-lg shadow-md" ref={myVideoRef} autoPlay />
+                    <button onClick={() => setShowBottomRightVideo(!showBottomRightVideo)} className="absolute top-[42%] left-[10px] duration-300">
+                        {showBottomRightVideo ? <ArrowForwardIosIcon style={{ color: "white" }} /> : <ArrowBackIosIcon style={{ color: "white" }} />}
+                    </button>
+                </div>
+            </div>
+            {/* <div className="flex flex-col items-center absolute bottom-10">
         <input
           className="px-4 py-2 mb-2 text-gray-800 border-2 border-gray-200 rounded-lg"
           type="text"
@@ -135,12 +123,12 @@ const VideoCall = () => {
           {isCopied ? "Copied!" : "Copy ID"}
         </button>
       </div> */}
-      <TopRightButtons />
-      <TopLeftIntro />
-      <MainButtons {...{ audio, setAudio, video, setVideo, bgColor }} />
-      {video && <MovableComponent {...{ videoRef: myMobileVideoRef }} />}
-    </div>
-  );
+            <TopRightButtons />
+            <TopLeftIntro />
+            <MainButtons {...{ audio, setAudio, video, setVideo, bgColor }} />
+            {video && <MovableComponent {...{ videoRef: myMobileVideoRef }} />}
+        </div>
+    );
 };
 
 export default VideoCall;
