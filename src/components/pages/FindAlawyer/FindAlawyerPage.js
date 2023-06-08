9bb77041b640d658d95e0ef4c6e627f73b3412c8
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2023-06-08 15:52:28 +0600

    added: footer in find professionals

--- src/components/pages/FindAlawyer/FindAlawyerPage.js
+++ src/components/pages/FindAlawyer/FindAlawyerPage.js
@@ -5,18 +5,22 @@ import { Outlet } from "react-router";
 // Components
 import { FindLawyerKaziAgentHeader } from "../../shared/Components/FindLawyerKaziAgent/FindLawyerKaziAgentHeader";
 import CustomHeader from "../../shared/CustomHeader/CustomHeader";
+import Footer from "../../shared/Footer/Footer";
 const FindAlawyerPage = () => {
-  return (
-    <>
-      <CustomHeader title="Find Lawyer" />
-      <div className="max-w-[1200px] mx-auto flex flex-col mt-5 min-h-screen">
-        <FindLawyerKaziAgentHeader page="lawyer" />
-        <div className="h-full w-full mt-[20px] px-[20px]">
-          <Outlet />
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <CustomHeader title="Find Lawyer" />
+            <div className="max-w-[1200px] mx-auto flex flex-col mt-5 min-h-screen">
+                <FindLawyerKaziAgentHeader page="lawyer" />
+                <div className="h-full w-full mt-[20px] px-[20px]">
+                    <Outlet />
+                </div>
+            </div>
+            <div className="mt-[50px]">
+                <Footer />
+            </div>
+        </>
+    );
 };
 
 export default FindAlawyerPage;
