0b329b2c3bb77a76a78c65aafaa2f99129667ba0
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2023-06-08 08:01:29 +0600

    solved: matrimony21

--- src/Wrapper/Home/mobileversion/BottomNav.js
+++ src/Wrapper/Home/mobileversion/BottomNav.js
@@ -11,49 +11,41 @@ import Shop from "../../../assets/images/Logo/Shop.svg";
 import "../mobileversion/TopMenu.css";
 
 export const BottomNav = () => {
-  return (
-    <div className="rounded bg-bottom-nav z-50 max-w-[1024px] mx-auto">
-      <div className="flex items-center justify-between px-4">
-        <NavLink to="/mobileUpgrade">
-          {({ isActive }) => (
-            <img
-              src={member}
-              alt="Not Available"
-              className={`w-10 mt-1 ${
-                isActive &&
-                "-mt-14 border-4 border-[#F8F8FF] bg-[#E8E8E8] rounded-full w-14"
-              }`}
-            ></img>
-          )}
-        </NavLink>
-        <NavLink to="/">
-          {({ isActive }) => (
-            <img
-              src="https://i.ibb.co/d5yyNd6/512-512-Icon.png"
-              alt="Not Available"
-              className={`w-12 ${
-                isActive &&
-                "-mt-14 border-4 border-[#EBDEF4] rounded-full w-14 pl-[2px] bg-[#E8E8E8]"
-              }`}
-            ></img>
-          )}
-        </NavLink>
-        <NavLink>
-          <img src={Shop} alt="Not Available" className="w-10 mt-1"></img>
-        </NavLink>
-        <NavLink to="/setting">
-          {({ isActive }) => (
-            <img
-              src={cart}
-              alt="Not Available"
-              className={`w-10 mt-1 ${
-                isActive &&
-                "-mt-14 border-4 border-white rounded-full w-14 bg-[#E8E8E8]"
-              }`}
-            ></img>
-          )}
-        </NavLink>
-      </div>
-    </div>
-  );
+    return (
+        <div className="rounded bg-bottom-nav z-50 max-w-[1024px] mx-auto">
+            <div className="flex items-center justify-between px-4">
+                <NavLink to="/mobileUpgrade">
+                    {({ isActive }) => (
+                        <img
+                            src={member}
+                            alt="Not Available"
+                            className={`w-10 mt-1 ${isActive && "-mt-14 border-4 border-[#F8F8FF] bg-[#E8E8E8] rounded-full w-14"}`}
+                        ></img>
+                    )}
+                </NavLink>
+                <NavLink to="/">
+                    {({ isActive }) => (
+                        <img
+                            src="https://i.ibb.co/d5yyNd6/512-512-Icon.png"
+                            alt="Not Available"
+                            className={`w-12 ${isActive && "-mt-14 border-4 border-[#EBDEF4] rounded-full w-14 pl-[2px] bg-[#E8E8E8]"}`}
+                        ></img>
+                    )}
+                </NavLink>
+                <a href="https://shop.shongshari.com" target="_blank" rel="noreferrer">
+                    <img src={Shop} alt="Not Available" className={`w-12`}></img>
+                    {/* <img src={Shop} alt="Not Available" className="w-10 mt-1"></img> */}
+                </a>
+                <NavLink to="/setting">
+                    {({ isActive }) => (
+                        <img
+                            src={cart}
+                            alt="Not Available"
+                            className={`w-10 mt-1 ${isActive && "-mt-14 border-4 border-white rounded-full w-14 bg-[#E8E8E8]"}`}
+                        ></img>
+                    )}
+                </NavLink>
+            </div>
+        </div>
+    );
 };
