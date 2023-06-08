3607d582b52c6abf2af60738da025f4adc371ddf
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2023-06-08 07:37:47 +0600

    solved: added video biodata upload card

--- src/components/SingleProfilesUser/MobileSingleProfilesUser/MobileSingleProfilesUser.js
+++ src/components/SingleProfilesUser/MobileSingleProfilesUser/MobileSingleProfilesUser.js
@@ -10,33 +10,28 @@ import { MobileBackButton } from "../../shared/Components/MobileBackButton";
 import { Badges } from "../Badges";
 import { PhotoUploadCard } from "../PhotoUploadCard";
 import { VerificationCard } from "../VerificationCard";
+import VideoBioUploadCard from "../VideoBioUploadCard/VideoBioUploadCard";
 import { MobileAccordionLink } from "./MobileAccordionLink";
 import MobileUsersProfile from "./MobileUsersProfile";
 
 const MobileSingleProfilesUser = () => {
-  const { data: badgeData, error: badgeError } = useGetUserBadgesQuery();
-  // js variable declaration
-  return (
-    <section className="max-w-[1024px] mx-auto">
-      <MobileBackButton name="Profile" />
-      <MobileUsersProfile />
-      <VerificationCard
-        title={false}
-        mt="0px"
-        height="100px"
-        responsive={true}
-      />
-      {badgeData?.data?.length !== 0 && (
-        <Badges data={badgeData} error={badgeError} />
-      )}
-      <PhotoUploadCard />
-      <div className="py-[20px]">
-        <MobileAccordionLink></MobileAccordionLink>
-      </div>
-      <div className="h-8"></div>
-      <BottomNav></BottomNav>
-    </section>
-  );
+    const { data: badgeData, error: badgeError } = useGetUserBadgesQuery();
+    // js variable declaration
+    return (
+        <section className="max-w-[1024px] mx-auto">
+            <MobileBackButton name="Profile" />
+            <MobileUsersProfile />
+            <VerificationCard title={false} mt="0px" height="100px" responsive={true} />
+            {badgeData?.data?.length !== 0 && <Badges data={badgeData} error={badgeError} />}
+            <VideoBioUploadCard />
+            <PhotoUploadCard />
+            <div className="py-[20px]">
+                <MobileAccordionLink></MobileAccordionLink>
+            </div>
+            <div className="h-8"></div>
+            <BottomNav></BottomNav>
+        </section>
+    );
 };
 
 export default MobileSingleProfilesUser;
