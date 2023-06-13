ceea34d6aa5e94e4a41dff86fb835a1e34b88fce
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2023-06-14 05:50:54 +0600

    added: package

--- src/components/pages/packages/CompleteFeaturePackages.js
+++ src/components/pages/packages/CompleteFeaturePackages.js
@@ -7,72 +7,63 @@ import React, { useEffect, useState } from "react";
 
 // Components
 import { useGetProfileDetailsWIthAuthQuery } from "../../../Redux/features/userInfo/userApi";
-import { SuccessSnackBar } from "../../ui/error/snackBar/SuccessSnackBar";
-import PackageCard from "../../shared/Packages/PackageCard";
-import { ServerErrorMessage } from "../../ui/error/ServerErrorMessage";
 import { OvalLoader } from "../../shared/Cards/Loader/OvalLoader/OvalLoader";
+import { PackageTestCard } from "../../shared/Cards/Packages/PackageTestCard";
+import { ServerErrorMessage } from "../../ui/error/ServerErrorMessage";
+import { SuccessSnackBar } from "../../ui/error/snackBar/SuccessSnackBar";
 
 const CompleteFeaturePackages = () => {
-  const [packages, setPackages] = useState([]);
-  const [successSnackBarOpen, setSuccessSnackBarOpen] = useState(false);
-  // const navigate = useNavigate();
-  const { data, isLoading, error } = useGetAllPackagesQuery();
-    console.log(packages);
-  const { data: profile } = useGetProfileDetailsWIthAuthQuery();
+    const [packages, setPackages] = useState([]);
+    const [successSnackBarOpen, setSuccessSnackBarOpen] = useState(false);
+    // const navigate = useNavigate();
+    const { data, isLoading, error } = useGetAllPackagesQuery();
+    const { data: profile } = useGetProfileDetailsWIthAuthQuery();
 
-  useEffect(() => {
-    if (data) {
-      const newPackages = [...data?.data?.packages].filter(
-        (item) => item.category === "complete-feature"
-      );
-      setPackages([...newPackages].sort((a, b) => a.priceMonth - b.priceMonth));
-    }
-  }, [data]);
+    useEffect(() => {
+        if (data) {
+            const newPackages = [...data?.data?.packages].filter(item => item.category === "complete-feature");
+            setPackages([...newPackages].sort((a, b) => a.priceMonth - b.priceMonth));
+        }
+    }, [data]);
 
-  return (
-    <div className="max-w-[1200px] mx-auto mt-32">
-      <h6 className="text-[#5650ce] text-center text-5xl font-semibold font-Inter mb-6">
-        Complete feature packages
-      </h6>
+    return (
+        <div className="max-w-[1200px] mx-auto mt-32">
+            <h6 className="text-[#5650ce] text-center text-5xl font-semibold font-Inter mb-6">Complete feature packages</h6>
 
-      {isLoading && (
-        <div className="my-16">
-          <OvalLoader />
-        </div>
-      )}
+            {isLoading && (
+                <div className="my-16">
+                    <OvalLoader />
+                </div>
+            )}
 
-      {error && (
-        <div className="flex items-center justify-center my-16">
-          <ServerErrorMessage />
-        </div>
-      )}
-      {packages.length !== 0 && (
-        <div className="px-auto grid md:grid-cols-2 my-16 lg:grid-cols-3 gap-4">
-          {packages?.map((pack, i) => (
-            <PackageCard
-              pack={pack}
-              packages={packages}
-              index={i}
-              user={profile?._id}
-              setSuccessSnackBarOpen={setSuccessSnackBarOpen}
+            {error && (
+                <div className="flex items-center justify-center my-16">
+                    <ServerErrorMessage />
+                </div>
+            )}
+            {packages.length !== 0 && (
+                <div className="px-auto grid md:grid-cols-2 my-16 lg:grid-cols-3 gap-4">
+                    {packages?.map((pack, i) => (
+                        <PackageTestCard
+                            pack={pack}
+                            packages={packages}
+                            index={i}
+                            user={profile?._id}
+                            setSuccessSnackBarOpen={setSuccessSnackBarOpen}
+                        />
+                    ))}
+                </div>
+            )}
+            {packages?.length === 0 && data && <div className="text-center text-3xl font-bold text-gray-400 my-16">No Packages Found</div>}
+            <SuccessSnackBar
+                {...{
+                    successSnackBarOpen,
+                    setSuccessSnackBarOpen,
+                    message: "Successful",
+                }}
             />
-          ))}
         </div>
-      )}
-      {packages?.length === 0 && data && (
-        <div className="text-center text-3xl font-bold text-gray-400 my-16">
-          No Packages Found
-        </div>
-      )}
-      <SuccessSnackBar
-        {...{
-          successSnackBarOpen,
-          setSuccessSnackBarOpen,
-          message: "Successful",
-        }}
-      />
-    </div>
-  );
+    );
 };
 
 export default CompleteFeaturePackages;
