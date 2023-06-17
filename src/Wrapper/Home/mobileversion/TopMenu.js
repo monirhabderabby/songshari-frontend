a7dd87490907103f58ae978a53ac6f6b590625a4
mobashirul-alam <mobashirulalamseam@gmail.com>
2023-06-17 09:04:03 +0600

    profile photo decision at top menu

--- src/Wrapper/Home/mobileversion/TopMenu.js
+++ src/Wrapper/Home/mobileversion/TopMenu.js
@@ -18,6 +18,7 @@ import { loadUserData } from "../../../Redux/features/userInfo/userInfo";
 
 // css import;
 import "./TopMenu.css";
+import customFunc from "../../../assets/utilities/customFunc";
 
 export const TopMenu = () => {
   // hooks variables
@@ -26,9 +27,11 @@ export const TopMenu = () => {
     (state) => state?.persistedReducer?.userInfo?.userInfo?.user
   );
   let { profilePhoto } = userInfo || {};
-  profilePhoto = profilePhoto
-    ? profilePhoto
-    : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
+
+  // profile photo decision maker
+  const { profilePhotoDecisionMaker } = customFunc;
+  profilePhoto = profilePhotoDecisionMaker(profilePhoto);
+
   let name;
   if (userInfo?.firstName) {
     name = userInfo?.firstName || "Empty";
