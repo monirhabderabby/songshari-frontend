d6c335118d866e36c3b60707b3829d087ab2a402
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2023-06-23 00:03:22 +0600

    fixed

--- src/components/Features/Messege/Messeges/MessegeLeft.js
+++ src/components/Features/Messege/Messeges/MessegeLeft.js
@@ -1,34 +1,24 @@
-import React, { useEffect, useRef } from "react";
+import { motion } from "framer-motion";
 import moment from "moment";
+import React, { useEffect, useRef } from "react";
 import customFunc from "../../../../assets/utilities/customFunc";
-export const MessegeLeft = ({ profile, message }) => {
-  // profile photo decision maker
-  const { profilePhotoDecisionMaker } = customFunc;
+export const MessegeLeft = ({ profile, message, index }) => {
+    // profile photo decision maker
+    const { profilePhotoDecisionMaker } = customFunc;
 
-  const scrollRef = useRef();
-  useEffect(() => {
-    scrollRef.current.scrollIntoView({ behaviour: "smooth" });
-  });
-  return (
-    <div
-      ref={scrollRef}
-      className={`flex w-full justify-start gap-x-[20px] my-[51px]`}
-    >
-      <img
-        className="h-[40px] w-[40px] rounded-full"
-        src={profilePhotoDecisionMaker(profile?.profilePhoto)}
-        alt="profile"
-      />
-      <div className="max-w-[60%]">
-        <div className="content bg-[#F2F2F2] rounded-br-[20px] rounded-tr-[20px] rounded-bl-[20px] px-[20px] py-[9px] ">
-          <p className="text-[#000000] text-[16px] font-normal">
-            {message?.message}
-          </p>
-        </div>
-        <p className="text-[#969696] text-[12px] font-normal mt-[13px]">
-          {moment(message?.time).format("LT")}
-        </p>
-      </div>
-    </div>
-  );
+    const scrollRef = useRef();
+    useEffect(() => {
+        scrollRef.current.scrollIntoView({ behaviour: "smooth" });
+    });
+    return (
+        <motion.div ref={scrollRef} className={`flex w-full justify-start gap-x-[20px] my-[51px]`}>
+            <img className="h-[40px] w-[40px] rounded-full" src={profilePhotoDecisionMaker(profile?.profilePhoto)} alt="profile" />
+            <div className="max-w-[60%]">
+                <div className="content bg-[#F2F2F2] rounded-br-[20px] rounded-tr-[20px] rounded-bl-[20px] px-[20px] py-[9px] ">
+                    <p className="text-[#000000] text-[16px] font-normal">{message?.message}</p>
+                </div>
+                <p className="text-[#969696] text-[12px] font-normal mt-[13px]">{moment(message?.time).format("LT")}</p>
+            </div>
+        </motion.div>
+    );
 };
--- src/components/Features/Messege/Messeges/MessegeRight.js
+++ src/components/Features/Messege/Messeges/MessegeRight.js
@@ -1,34 +1,24 @@
-import React, { useEffect, useRef } from "react";
+import { motion } from "framer-motion";
 import moment from "moment";
+import React, { useEffect, useRef } from "react";
 import customFunc from "../../../../assets/utilities/customFunc";
-export const MessegeRight = ({ profile, message }) => {
-  // profile photo decision maker
-  const { profilePhotoDecisionMaker } = customFunc;
+export const MessegeRight = ({ profile, message, index }) => {
+    // profile photo decision maker
+    const { profilePhotoDecisionMaker } = customFunc;
 
-  const scrollRef = useRef();
-  useEffect(() => {
-    scrollRef.current.scrollIntoView({ behaviour: "smooth" });
-  });
-  return (
-    <div
-      ref={scrollRef}
-      className="flex w-full justify-end gap-x-[20px] my-[51px]"
-    >
-      <div className="flex flex-col flex-wrap-reverse">
-        <div className="content bg-[#A32BCA] rounded-tl-[20px] rounded-tr-[20px] rounded-bl-[20px] px-[20px] py-[9px] w-auto">
-          <p className="text-white text-[16px] font-normal">
-            {message?.message}
-          </p>
-        </div>
-        <p className="text-[#969696] text-[12px] text-right font-normal mt-[13px]">
-          {moment(message?.time).format("LT")}
-        </p>
-      </div>
-      <img
-        className="h-[40px] w-[40px] mt-1 rounded-full"
-        src={profilePhotoDecisionMaker(profile?.profilePhoto)}
-        alt="profile"
-      />
-    </div>
-  );
+    const scrollRef = useRef();
+    useEffect(() => {
+        scrollRef.current.scrollIntoView({ behaviour: "smooth" });
+    });
+    return (
+        <motion.div ref={scrollRef} className="flex w-full justify-end gap-x-[20px] my-[51px]">
+            <div className="flex flex-col flex-wrap-reverse">
+                <div className="content bg-[#A32BCA] rounded-tl-[20px] rounded-tr-[20px] rounded-bl-[20px] px-[20px] py-[9px] w-auto">
+                    <p className="text-white text-[16px] font-normal">{message?.message}</p>
+                </div>
+                <p className="text-[#969696] text-[12px] text-right font-normal mt-[13px]">{moment(message?.time).format("LT")}</p>
+            </div>
+            <img className="h-[40px] w-[40px] mt-1 rounded-full" src={profilePhotoDecisionMaker(profile?.profilePhoto)} alt="profile" />
+        </motion.div>
+    );
 };
