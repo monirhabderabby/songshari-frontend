0ab276bcac0d8327eec5e5df7b837345aa62d22d
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2023-06-23 22:41:18 +0600

    added: animation on happy stories

--- src/components/pages/SuccessStory/SuccessStoryGlobalContainer/SingleSuccessStoryDetailsPage/SingleSuccessStoryImages/SingleSuccessStoryImagesCard.js
+++ src/components/pages/SuccessStory/SuccessStoryGlobalContainer/SingleSuccessStoryDetailsPage/SingleSuccessStoryImages/SingleSuccessStoryImagesCard.js
@@ -1,25 +1,22 @@
 import React from "react";
+import VarientAnimation from "../../../../../../assets/utilities/Animation/VarientAnimation";
 
-const SingleSuccessStoryImagesCard = ({
-  setSelectedImage,
-  selectedImage,
-  attachments,
-}) => {
-  return (
-    <div className="flex justify-center gap-2">
-      {attachments?.map((img) => (
-        <img
-          className={`${
-            selectedImage === img?.url && "border-2 border-red-500"
-          } w-[96px] h-[74px] rounded-md mb-6`}
-          key={img?._id}
-          src={img?.url}
-          alt=""
-          onClick={() => setSelectedImage(img?.url)}
-        />
-      ))}
-    </div>
-  );
+const SingleSuccessStoryImagesCard = ({ setSelectedImage, selectedImage, attachments }) => {
+    return (
+        <div className="flex justify-center gap-2">
+            {attachments?.map((img, i) => (
+                <VarientAnimation direction="up" delay={0.3}>
+                    <img
+                        className={`${selectedImage === img?.url && "border-2 border-red-500"} w-[96px] h-[74px] rounded-md mb-6`}
+                        key={img?._id}
+                        src={img?.url}
+                        alt=""
+                        onClick={() => setSelectedImage(img?.url)}
+                    />
+                </VarientAnimation>
+            ))}
+        </div>
+    );
 };
 
 export default SingleSuccessStoryImagesCard;
