41ef2f4b42507d56439ad64054e852cc74149857
Md Robin Hossain <hossainmdrobin967@gmail.com>
2023-06-26 02:27:12 +0600

    ss

--- src/components/shared/Cards/SwipeAndMatch/SwipAndMatchCard.js
+++ src/components/shared/Cards/SwipeAndMatch/SwipAndMatchCard.js
@@ -97,11 +97,11 @@ export const SwipAndMatchCard = ({
 
   return (
     <div className={`max-w-[280px] w-full  h-[340px]  absolute overflow-hidden`}>
-      {rejected && <h1 className="gradientTextWithBorderRight animate-bounce">Nope</h1>}
+      {rejected && <h1 className="gradientTextWithBorderRight animate-bounce -rotate-45">Nope</h1>}
 
-      {liked && (
-        <h1 className="gradientTextWithBorderLeft animate-bounce">Liked</h1>
-      )}
+      {/* {liked && ( */}
+        <h1 className="gradientTextWithBorderLeft -rotate-45">Liked</h1>
+      {/* )} */}
 
       <img
         src={profilePhotoDecisionMaker(data?.profilePhoto)}
